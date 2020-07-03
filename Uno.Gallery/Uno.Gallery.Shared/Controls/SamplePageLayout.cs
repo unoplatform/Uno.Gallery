409b879d85d95972fc79d0137528d4b79db0fc4d
Kevin Takla <kevin.takla@nventive.com>
2020-07-03 11:17:43 -0400

    add samplepagelayoutcontrol

---
+++ Uno.Gallery/Uno.Gallery.Shared/Controls/SamplePageLayout.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.Gallery.Controls
+{
+	public partial class SamplePageLayout : ContentControl
+	{
+		public string Title
+		{
+			get { return (string)GetValue(TitleProperty); }
+			set { SetValue(TitleProperty, value); }
+		}
+
+		public static readonly DependencyProperty TitleProperty =
+			DependencyProperty.Register("Title", typeof(string), typeof(SamplePageLayout), new PropertyMetadata(string.Empty));
+
+
+		public string Description
+		{
+			get { return (string)GetValue(DescriptionProperty); }
+			set { SetValue(DescriptionProperty, value); }
+		}
+
+		public static readonly DependencyProperty DescriptionProperty =
+			DependencyProperty.Register("Description", typeof(string), typeof(SamplePageLayout), new PropertyMetadata(string.Empty));
+	}
+}
