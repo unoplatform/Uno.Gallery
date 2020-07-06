e8592bfd69de8f14fed93ce7e9ecdc2e268ec006
Julie Cantin <julie.cantin@nventive.com>
2020-07-06 14:22:07 -0400

    Add navigation to tests

---
+++ Uno.Gallery/Uno.Gallery.Shared/Attributes/SamplePageAttribute.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno.Gallery
+{
+	[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+	public sealed class SamplePageAttribute : Attribute
+	{
+		public SamplePageAttribute(string title, string description, Type viewModelType)
+		{
+			Title = title;
+			Description = description;
+			ViewModelType = viewModelType;
+		}
+
+		public string Title { get; }
+
+		public string Description { get; }
+
+		public Type ViewModelType { get; }
+
+		public static IReadOnlyList<Sample> GetAllSamples()
+		{
+			var query = from type in FindDefinedAssemblies(Assembly.GetExecutingAssembly())
+						let sampleAttribute = FindSampleAttribute(type)
+						where sampleAttribute != null
+						select new Sample(sampleAttribute, type.AsType());
+
+			return query.ToArray();
+		}
+
+		private static IEnumerable<TypeInfo> FindDefinedAssemblies(Assembly assembly)
+		{
+			try
+			{
+				return assembly.DefinedTypes.ToArray();
+			}
+			catch (Exception)
+			{
+				return new TypeInfo[0];
+			}
+		}
+
+		private static SamplePageAttribute FindSampleAttribute(TypeInfo type)
+		{
+			try
+			{
+				if (!(type.Namespace?.StartsWith("System.Windows") ?? true))
+				{
+					return type?.GetCustomAttributes()
+						.OfType<SamplePageAttribute>()
+						.FirstOrDefault();
+				}
+				return null;
+			}
+			catch (Exception)
+			{
+				return null;
+			}
+		}
+	}
+}
---
+++ Uno.Gallery/Uno.Gallery.UWP/Views/Samples/Sample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno.Gallery
+{
+	public class Sample
+	{
+		public Sample(SamplePageAttribute attribute, Type viewType)
+		{
+			Title = attribute.Title;
+			Description = attribute.Description;
+			ViewModelType = attribute.ViewModelType;
+			ViewType = viewType;
+		}
+
+		public string Title { get; }
+
+		public string Description { get; }
+
+		public Type ViewType { get; }
+
+		public Type ViewModelType { get; }
+	}
+}
