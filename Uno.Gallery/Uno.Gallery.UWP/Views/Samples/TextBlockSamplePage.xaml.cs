09e9d6c3d4e083f21847db7dbe7c6b3902c0c3e7
Julie Cantin <julie.cantin@nventive.com>
2020-07-08 13:37:20 -0400

    Add TextBlock sample
    Related Work Items: #187806, #188526

---
+++ Uno.Gallery/Uno.Gallery.UWP/Views/Samples/TextBlockSamplePage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Uno.Gallery.Views.Samples
+{
+	[SamplePage("TextBlock", "TextBlock sample")]
+	public sealed partial class TextBlockSamplePage : Page
+	{
+		public TextBlockSamplePage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
