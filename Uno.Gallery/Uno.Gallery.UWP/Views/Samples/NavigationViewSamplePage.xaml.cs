b187671fb06b319c4de39a5eddc09482b024f7be
Julie Cantin <julie.cantin@nventive.com>
2020-07-09 09:32:09 -0400

    Add navigation view sample

---
+++ Uno.Gallery/Uno.Gallery.UWP/Views/Samples/NavigationViewSamplePage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Uno.Gallery.Views.Samples
+{
+	[SamplePage("NavigationView", "NavigationView sample")]
+	public sealed partial class NavigationViewSamplePage : Page
+	{
+		public NavigationViewSamplePage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
