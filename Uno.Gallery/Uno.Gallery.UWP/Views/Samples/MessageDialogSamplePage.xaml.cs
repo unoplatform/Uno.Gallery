7ee89841e583366b093207b6f17c39ae36a23547
Carl-Henry Mathieu <ch.mathieu@nventive.com>
2020-07-17 21:27:31 -0400

    Message Dialog

--- Uno.Gallery/Uno.Gallery.UWP/Views/Samples/MessageDialogSamplePage.xaml.cs
+++ Uno.Gallery/Uno.Gallery.UWP/Views/Samples/MessageDialogSamplePage.xaml.cs
@@ -22,5 +22,31 @@ namespace Uno.Gallery.Views.Samples
 		{
 			this.InitializeComponent();
 		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			var messageDialog = new Windows.UI.Popups.MessageDialog("Hello world!");
+			messageDialog.ShowAsync();
+		}
+
+		private void Button_Click2(object sender, RoutedEventArgs e)
+		{
+			var messageDialog = new Windows.UI.Popups.MessageDialog("This is a very important message.", "Notice");
+			messageDialog.ShowAsync();
+		}
+
+		private void Button_Click3(object sender, RoutedEventArgs e)
+		{
+			var messageDialog = new Windows.UI.Popups.MessageDialog("What is the answer to Life, The Universe and Everything?", "Ultimate Question")
+			{
+				//CancelCommandIndex = 0,
+				Commands =
+				{
+					new Windows.UI.Popups.UICommand("42"),
+					new Windows.UI.Popups.UICommand("Not 42"),
+				}
+			};
+			messageDialog.ShowAsync();
+		}
 	}
 }
