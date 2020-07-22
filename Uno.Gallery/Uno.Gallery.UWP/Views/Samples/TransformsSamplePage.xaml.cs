e637598e8f51d9f236f382ec002cbc064685feee
Nicolas Champagne <nicolas.champagne@nventive.com>
2020-07-22 09:33:49 -0400

    feature: Added Transforms page (legacy)

---
+++ Uno.Gallery/Uno.Gallery.UWP/Views/Samples/TransformsSamplePage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Uno.Gallery.Views.Samples
+{
+	[SamplePage("Transforms", "Transforms sample", SourceSdk.WinUI)]
+	public sealed partial class TransformsSamplePage : Page
+	{
+		public TransformsSamplePage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
