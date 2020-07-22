17ce289d933237e650ed3690ada792c19d0aec83
Nicolas Champagne <nicolas.champagne@nventive.com>
2020-07-22 11:16:34 -0400

    feature: Added WebView page (legacy)

---
+++ Uno.Gallery/Uno.Gallery.UWP/Views/Samples/WebViewSamplePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Uno.Gallery.Views.Samples
+{
+	// There is no WebView implementation in WebAssembly
+#if !__WASM__
+	[SamplePage("WebView", "WebView sample", SourceSdk.WinUI)]
+#endif
+	public sealed partial class WebViewSamplePage : Page
+	{
+		public WebViewSamplePage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
