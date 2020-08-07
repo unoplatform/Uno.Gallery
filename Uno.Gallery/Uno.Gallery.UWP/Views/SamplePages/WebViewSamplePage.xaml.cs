693647936feb9f7f4051aaea02bf9d6bcfa67567
Carl-Henry Mathieu <ch.mathieu@nventive.com>
2020-08-06 22:15:56 -0400

    Hide WebView for MacOS

--- Uno.Gallery/Uno.Gallery.UWP/Views/SamplePages/WebViewSamplePage.xaml.cs
+++ Uno.Gallery/Uno.Gallery.UWP/Views/SamplePages/WebViewSamplePage.xaml.cs
@@ -2,8 +2,8 @@
 
 namespace Uno.Gallery.Views.Samples
 {
-	// There is no WebView implementation in WebAssembly
-#if !__WASM__
+	// There is no WebView implementation in WebAssembly and MacOS
+#if !__WASM__ && !__MACOS__
 	[SamplePage(SampleCategory.Features, "WebView", Description = "This control hosts a web page or HTML content within an application.")]
 #endif
 	public sealed partial class WebViewSamplePage : Page
