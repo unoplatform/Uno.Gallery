e94f75afa68ac9f29507c5689f089a53f2270fe7
Jean-Philippe Levesque <jp.levesque@nventive.com>
2020-08-07 11:58:51 -0400

    Change the name of the palette menu items

--- Uno.Gallery/Uno.Gallery.UWP/Views/GeneralPages/FluentPalettePage.xaml.cs
+++ Uno.Gallery/Uno.Gallery.UWP/Views/GeneralPages/FluentPalettePage.xaml.cs
@@ -2,7 +2,7 @@
 
 namespace Uno.Gallery
 {
-	[SamplePage(SampleCategory.Theme, "Palette for Fluent", SortOrder = 2, Description = Description)]
+	[SamplePage(SampleCategory.Theme, "Fluent Palette", SortOrder = 2, Description = Description)]
 	public sealed partial class FluentPalettePage : Page
 	{
 		private const string Description = "View the Uno palette applied to Fluent's styles.";
