8e869034af910a9bac47aa1d0d9ff046fd2e98d2
Maxime Dion <maxime.dion@nventive.com>
2020-08-25 08:29:34 -0400

    ViewBox sample

---
+++ Uno.Gallery/Uno.Gallery.UWP/Views/SamplePages/ViewBoxSamplePage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Uno.Gallery.Views.Samples
+{
+	[SamplePage(SampleCategory.Features, "ViewBox", Description = "Defines a content decorator that can stretch and scale a single child to fill the available space.", DocumentationLink = "https://docs.microsoft.com/en-us/uwp/api/windows.ui.xaml.controls.viewbox")]
+	public sealed partial class ViewBoxSamplePage : Page
+	{
+		public ViewBoxSamplePage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
