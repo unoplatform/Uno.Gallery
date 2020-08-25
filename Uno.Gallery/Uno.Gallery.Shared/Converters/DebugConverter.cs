c8b84f28d5e68b69fe1f4b28d067137b030ea4dd
Maxime Dion <maxime.dion@nventive.com>
2020-08-25 11:34:15 -0400

    Add ListView selection sample

---
+++ Uno.Gallery/Uno.Gallery.Shared/Converters/DebugConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Data;
+
+namespace Uno.Gallery.Converters
+{
+	/// <summary>
+	/// Use this converter to debug data bindings in your xaml.
+	/// </summary>
+	public class DebugConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			// Put a breakpoint here to inspect values from the ViewModel to the View.
+			return value;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			// Put a breakpoint here to inspect values from the View to the ViewModel.
+			return value;
+		}
+	}
+}
