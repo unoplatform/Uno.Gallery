519cff3e0631c822df0981bbeeb42c2c98832d84
Maxime Dion <maxime.dion@nventive.com>
2020-09-03 14:06:31 -0400

    Data Binding sample

---
+++ Uno.Gallery/Uno.Gallery.UWP/Views/SamplePages/BindingSamplePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Uno.Gallery.Views.Samples
+{
+	[SamplePage(SampleCategory.Features, "Binding", Description = "Bindings allow you to pass data between your UI and business logic.", DocumentationLink = "https://docs.microsoft.com/en-us/windows/uwp/data-binding/")]
+	public sealed partial class BindingSamplePage : Page
+	{
+		public BindingSamplePage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
