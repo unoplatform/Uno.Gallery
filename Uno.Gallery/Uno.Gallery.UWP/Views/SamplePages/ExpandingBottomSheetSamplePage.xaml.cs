865214775968215b358d1524b9b807e3835df41d
Nicolas Champagne <nicolas.champagne@nventive.com>
2020-09-09 15:15:32 -0400

    feat: ExpandingBottomSheet

---
+++ Uno.Gallery/Uno.Gallery.UWP/Views/SamplePages/ExpandingBottomSheetSamplePage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Uno.Gallery.Views.SamplePages
+{
+	[SamplePage(SampleCategory.Components, "ExpandingBottomSheet", SourceSdk.UnoMaterial, Description = "This control allows users to toggle optional page content.", DocumentationLink = "https://material.io/components/sheets-bottom#expanding-bottom-sheet")]
+
+	public sealed partial class ExpandingBottomSheetSamplePage : Page
+	{
+		public ExpandingBottomSheetSamplePage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
