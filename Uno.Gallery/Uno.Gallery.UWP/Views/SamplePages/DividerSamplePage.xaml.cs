d1719b5a7ac7017ac75a4362dff764f938137cc9
Kevin Takla <kevin.takla@nventive.com>
2020-09-14 09:55:20 -0400

    add divide sample

---
+++ Uno.Gallery/Uno.Gallery.UWP/Views/SamplePages/DividerSamplePage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Uno.Gallery.Entities.Data;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Uno.Gallery.Views.Samples
+{
+	[SamplePage(
+		category: SampleCategory.Components,
+		title: "Divider",
+		Description = "This control is a thin line than can be used to divide layouts or groups content inside of lists.",
+		DocumentationLink = "https://material.io/components/dividers",
+		DataType = typeof(DividerItems)
+	)]
+	public sealed partial class DividerSamplePage : Page
+	{
+		public DividerSamplePage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
