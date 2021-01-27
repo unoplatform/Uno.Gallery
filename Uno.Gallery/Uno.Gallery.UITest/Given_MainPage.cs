719e8e87d680fad5536666f26b687672475e4c59
Jerome Laban <jerome.laban@nventive.com>
2021-01-27 09:14:25 -0500

    feat: Enable UI Tests

---
+++ Uno.Gallery/Uno.Gallery.UITest/Given_MainPage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Uno.UITest.Helpers.Queries;
+using Query = System.Func<Uno.UITest.IAppQuery, Uno.UITest.IAppQuery>;
+
+namespace Uno.Gallery.UITests
+{
+    public class Given_MainPage : TestBase
+    {
+        [Test]
+        public void When_SmokeTest()
+        {
+            Query ContainedSelector = q => q.All().Marked("MaterialContainedButton");
+            Query section = q => q.All().Marked("Section_Overview");
+
+            App.WaitForElement(section);
+            App.Tap(section);
+
+            App.WaitForElement(ContainedSelector);
+            App.Tap(ContainedSelector);
+
+            TakeScreenshot("Opened");
+        }
+    }
+}
---
+++ build/scripts/wasm-uitest-run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+export UNO_UITEST_TARGETURI=http://localhost:5000
+export UNO_UITEST_DRIVERPATH_CHROME=$BUILD_SOURCESDIRECTORY/build/node_modules/chromedriver/lib/chromedriver
+export UNO_UITEST_CHROME_BINARY_PATH=$BUILD_SOURCESDIRECTORY/build/node_modules/puppeteer/.local-chromium/linux-756035/chrome-linux/chrome
+export UNO_UITEST_SCREENSHOT_PATH=$BUILD_ARTIFACTSTAGINGDIRECTORY/screenshots/wasm
+export UNO_UITEST_PLATFORM=Browser
+export UNO_UITEST_CHROME_CONTAINER_MODE=true
+export UNO_UITEST_PROJECT=$BUILD_SOURCESDIRECTORY/Uno.Gallery/Uno.Gallery.UITest/Uno.Gallery.UITest.csproj
+export UNO_UITEST_BINARY=$BUILD_SOURCESDIRECTORY/Uno.Gallery/Uno.Gallery.UITest/bin/Release/net47/Uno.Gallery.UITest.dll
+export UNO_UITEST_LOGFILE=$BUILD_ARTIFACTSTAGINGDIRECTORY/screenshots/wasm/nunit-log.txt
+export UNO_UITEST_WASM_PROJECT=$BUILD_SOURCESDIRECTORY/Uno.Gallery/Uno.Gallery.Wasm/Uno.Gallery.Wasm.csproj
+export UNO_UITEST_WASM_OUTPUT_PATH=$BUILD_SOURCESDIRECTORY/Uno.Gallery/Uno.Gallery.Wasm/bin/Release/net5/dist/
+export UNO_UITEST_NUNIT_VERSION=3.10.0
+export UNO_UITEST_NUGET_URL=https://dist.nuget.org/win-x86-commandline/v5.7.0/nuget.exe
+
+cd $BUILD_SOURCESDIRECTORY
+
+msbuild /r /p:Configuration=Release $UNO_UITEST_PROJECT
+dotnet build /r /p:Configuration=Release $UNO_UITEST_WASM_PROJECT /p:IsUiAutomationMappingEnabled=True
+
+# Start the server
+dotnet run --project $UNO_UITEST_WASM_PROJECT -c Release --no-build &
+
+cd $BUILD_SOURCESDIRECTORY/build
+
+npm i chromedriver@83.0.1
+npm i puppeteer@3.1.0
+wget $UNO_UITEST_NUGET_URL
+mono nuget.exe install NUnit.ConsoleRunner -Version $UNO_UITEST_NUNIT_VERSION
+
+mkdir -p $UNO_UITEST_SCREENSHOT_PATH
+
+mono $BUILD_SOURCESDIRECTORY/build/NUnit.ConsoleRunner.$UNO_UITEST_NUNIT_VERSION/tools/nunit3-console.exe \
+  --trace=Verbose --inprocess --agents=1 --workers=1 \
+  $UNO_UITEST_BINARY || true
