c53b4e5370b016b5eeb6944da21ca4e77b4169fc
Steve Bilogan <steve.bilogan@gmail.com>
2021-03-12 10:57:09 -0500

    test: Add more ui tests

--- Uno.Gallery/Uno.Gallery.UITest/Given_RadioButton.cs
+++ Uno.Gallery/Uno.Gallery.UITest/Given_RadioButton.cs
@@ -1,17 +1,16 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NUnit.Framework;
 using Uno.UITest.Helpers.Queries;
-using Uno.UITests.Helpers;
 
 namespace Uno.Gallery.UITests
 {
-	public class Given_CheckBox : TestBase
+	public class Given_RadioButton : TestBase
 	{
-		protected string[] Sections => new[] { "Components", "CheckBox" };
+		protected string[] Sections => new[] { "Components", "RadioButton" };
 
 		[Test]
 		public void When_ClickMaterial()
@@ -22,7 +21,7 @@ namespace Uno.Gallery.UITests
 			TakeScreenshot("Before Checked");
 
 			var uncheckedBox = App.WaitThenTap(q => q.All().Marked("Material_Unchecked")).ToQueryEx();
-			
+
 			TakeScreenshot("After Checked");
 
 			Assert.IsTrue(uncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
