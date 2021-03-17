e7773042d712e649768f44f5b4da5ed304331d0f
Steve Bilogan <steve.bilogan@gmail.com>
2021-03-17 11:09:22 -0400

    feat(Cupertino): Add cupertino design theme

---
+++ Uno.Gallery/Uno.Gallery.UWP/Views/GeneralPages/CupertinoPalettePage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Uno.Gallery.Views.GeneralPages
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	[SamplePage(SampleCategory.Theme, "Cupertino Palette", SortOrder = 3, Description = Description)]
+	public sealed partial class CupertinoPalettePage : Page
+	{
+		private const string Description = "View the Uno palette applied to Cupertino's styles.";
+
+		public CupertinoPalettePage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
