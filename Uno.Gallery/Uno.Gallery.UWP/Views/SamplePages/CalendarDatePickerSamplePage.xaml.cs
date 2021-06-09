6b47e1d5a9806390bbb9aacfbc34d402dff8f880
Steve Bilogan <steve.bilogan@gmail.com>
2021-06-09 09:58:13 -0400

    fix(CalendarView): Separate CalendarView/CalendarDatePicker samples. Fix selection mode combobox

---
+++ Uno.Gallery/Uno.Gallery.UWP/Views/SamplePages/CalendarDatePickerSamplePage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Uno.Gallery.Views.SamplePages
+{
+	[SamplePage(SampleCategory.Components, "CalendarDatePicker", Description = "Represents a control that allows a user to pick a date from a calendar display.", DocumentationLink = "https://docs.microsoft.com/en-us/uwp/api/windows.ui.xaml.controls.calendardatepicker")]
+	public sealed partial class CalendarDatePickerSamplePage : Page
+	{
+		public CalendarDatePickerSamplePage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
