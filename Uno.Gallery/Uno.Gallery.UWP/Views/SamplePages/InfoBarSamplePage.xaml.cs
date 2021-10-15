6d9b2f8c169525eeeb9e393e3a50436c98104c2d
Agnès Zitte <agnes.zitte@nventive.com>
2021-10-15 11:06:24 -0400

    fix(InfoBar): Add InfoBar Samples

---
+++ Uno.Gallery/Uno.Gallery.UWP/Views/SamplePages/InfoBarSamplePage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace Uno.Gallery.Views.SamplePages
+{
+	[SamplePage(SampleCategory.Components, "InfoBar", Description = "This control is an inline notification for essential app-wide messages.", DocumentationLink = "https://docs.microsoft.com/en-us/windows/winui/api/microsoft.ui.xaml.controls.infobar?view=winui-2.5")]
+	public sealed partial class InfoBarSamplePage : Page
+	{
+		public InfoBarSamplePage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
