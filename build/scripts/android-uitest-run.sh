9b506607c02324faab775261f8cecadc2822585e
Jérôme Laban <jlaban@gmail.com>
2022-03-22 10:03:01 -0400

    ci: adjust android emulator restart

--- build/scripts/android-uitest-run.sh
+++ build/scripts/android-uitest-run.sh
@@ -10,19 +10,25 @@ export UNO_UITEST_ANDROID_PROJECT=$BUILD_SOURCESDIRECTORY/Uno.Gallery/Uno.Galler
 export UNO_UITEST_BINARY=$BUILD_SOURCESDIRECTORY/Uno.Gallery/Uno.Gallery.UITest/bin/Release/net47/Uno.Gallery.UITest.dll
 export UNO_UITEST_NUNIT_VERSION=3.11.1
 export UNO_UITEST_NUGET_URL=https://dist.nuget.org/win-x86-commandline/v5.7.0/nuget.exe
+export UNO_EMULATOR_INSTALLED=$BUILD_SOURCESDIRECTORY/build/.emulator_started
 
-# Install AVD files
-echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-28;google_apis_playstore;x86'
+if [ ! -f "$UNO_EMULATOR_INSTALLED" ];
+then
+	# Install AVD files
+	echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-28;google_apis_playstore;x86'
 
-# Create emulator
-echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n xamarin_android_emulator -k 'system-images;android-28;google_apis_playstore;x86' --force
+	# Create emulator
+	echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n xamarin_android_emulator -k 'system-images;android-28;google_apis_playstore;x86' --force
 
-echo $ANDROID_HOME/emulator/emulator -list-avds
+	echo $ANDROID_HOME/emulator/emulator -list-avds
 
-echo "Starting emulator"
+	echo "Starting emulator"
 
-# Start emulator in background
-$ANDROID_HOME/emulator/emulator -avd xamarin_android_emulator -no-snapshot > /dev/null 2>&1 &
+	# Start emulator in background
+	$ANDROID_HOME/emulator/emulator -avd xamarin_android_emulator -no-snapshot > /dev/null 2>&1 &
+
+	touch "$UNO_EMULATOR_INSTALLED"
+fi
 
 # build the sample, while the emulator is starting
 msbuild /r /p:Configuration=Release $UNO_UITEST_PROJECT
@@ -45,5 +51,3 @@ mkdir -p $UNO_UITEST_SCREENSHOT_PATH
 mono $BUILD_SOURCESDIRECTORY/build/NUnit.ConsoleRunner.$UNO_UITEST_NUNIT_VERSION/tools/nunit3-console.exe \
 	$UNO_UITEST_BINARY \
 	--timeout=2700000
-
-killall -HUP emulator
