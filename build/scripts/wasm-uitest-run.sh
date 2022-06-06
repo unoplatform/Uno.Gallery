473f17b6cab84175d71c2ab16f7cc4a1763b9bef
Jerome Laban <jerome.laban@nventive.com>
2022-06-06 11:41:47 -0400

    ci: Align wasm uitests

--- build/scripts/wasm-uitest-run.sh
+++ build/scripts/wasm-uitest-run.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 export UNO_UITEST_TARGETURI=http://localhost:5000
 export UNO_UITEST_DRIVERPATH_CHROME=$BUILD_SOURCESDIRECTORY/build/node_modules/chromedriver/lib/chromedriver
-export UNO_UITEST_CHROME_BINARY_PATH=$BUILD_SOURCESDIRECTORY/build/node_modules/puppeteer/.local-chromium/linux-1002410/chrome-linux/chrome
+export UNO_UITEST_CHROME_BINARY_PATH=$BUILD_SOURCESDIRECTORY/build/node_modules/puppeteer/.local-chromium/linux-991974/chrome-linux/chrome
 export UNO_UITEST_SCREENSHOT_PATH=$BUILD_ARTIFACTSTAGINGDIRECTORY/screenshots/wasm
 export UNO_UITEST_PLATFORM=Browser
 export UNO_UITEST_CHROME_CONTAINER_MODE=true
@@ -26,7 +26,7 @@ dotnet run --project $UNO_UITEST_WASM_PROJECT -c Release --no-build &
 
 cd $BUILD_SOURCESDIRECTORY/build
 
-npm i chromedriver@100.0.0
+npm i chromedriver@99.0.0
 npm i puppeteer@14.1.0
 wget $UNO_UITEST_NUGET_URL
 mono nuget.exe install NUnit.ConsoleRunner -Version $UNO_UITEST_NUNIT_VERSION
