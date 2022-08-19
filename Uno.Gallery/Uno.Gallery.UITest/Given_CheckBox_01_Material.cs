7d337de69f497b3cef8149dc6060622454189339
Sakshi Shukla <sakshi.shukla@nventive.com>
2022-08-19 14:26:47 -0400

    Ui Test: updated test script for checkbox controls(Material, Fluent, Cupertino)

--- Uno.Gallery/Uno.Gallery.UITest/Given_CheckBox_01_Material.cs
+++ Uno.Gallery/Uno.Gallery.UITest/Given_CheckBox_01_Material.cs
@@ -12,105 +12,178 @@ namespace Uno.Gallery.UITests
 	//[Ignore("WIP: M3 Migration")]
 	public class Given_CheckBox_01_Material : TestBase
 	{
-		    
-		/*This function is to test the Unchecked option  in checkbox for material */ 		
+
+		/*This function is to test the Unchecked option in checkbox for material */
 		[Test]
 		public void WhenMaterialClick_01_Unchecked()
 		{
+			//Navigation to Checkbox material control
 			NavigateToSample("CheckBox", "Material");
 
+			//Scrolling to Unchecked option
+			App.ScrollDownTo("Material_Unchecked");
+
+			//Initial Validation
 			TakeScreenshot("Before Checked");
 			var uncheckedBox = new QueryEx(x => x.All().Marked("Material_Unchecked"));
-			App.ScrollDownTo("Material_Unchecked");
+			Assert.AreEqual("UNCHECKED", uncheckedBox.GetDependencyPropertyValue("Content"));
+			Assert.IsFalse(uncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(uncheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Unchecked Box and taking screenshot
 			uncheckedBox.Tap();
 			TakeScreenshot("After Checked");
-			Assert.IsTrue(uncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));			
+
+			//validation after clicking Uncheckbox 
+			Assert.IsTrue(uncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(uncheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
 		}
-		     
+
 		/* This function is to test the DisabledUnchecked option  in checkbox for material.	*/
 		[Test]
 		public void WhenMaterialClick_02_DisabledUnchecked()
 		{
+			//Navigation to Checkbox material control
 			NavigateToSample("CheckBox", "Material");
 
+			//Scrolling to Disabled Unchecked option
+			App.ScrollDownTo("Material_Disabled_Unchecked");
+
+			//Initial Validation
 			TakeScreenshot("Before Checked");
 			var materialDisabledUncheckedBox = new QueryEx(x => x.All().Marked("Material_Disabled_Unchecked"));
-			App.ScrollDownTo("Material_Disabled_Unchecked");
+			Assert.AreEqual("DISABLED UNCHECKED", materialDisabledUncheckedBox.GetDependencyPropertyValue("Content"));
+			Assert.IsFalse(materialDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsFalse(materialDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Disabled Unchecked Box and taking screenshot
 			materialDisabledUncheckedBox.Tap();
 			TakeScreenshot("After Checked");
+
+			//validation after clicking Disabled Uncheckbox 			
+			Assert.IsFalse(materialDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
 			Assert.IsFalse(materialDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
-			Assert.IsFalse(materialDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));			
 		}
-		     
-		 /* This function is to test the Checked option in checkbox for material.	*/	 
+
+		/* This function is to test the Checked option in checkbox for material.	*/
 		[Test]
 		public void WhenMaterialClick_03_Checked()
 		{
+			//Navigation to Checkbox material control
 			NavigateToSample("CheckBox", "Material");
 
+			//Scrolling to Checked option
+			App.ScrollDownTo("Material_Checked");
+
+			//Initial Validation
 			TakeScreenshot("Before UnChecked");
 			var checkedBox = new QueryEx(x => x.All().Marked("Material_Checked"));
-			App.ScrollDownTo("Material_Checked");
+			Assert.AreEqual("CHECKED", checkedBox.GetDependencyPropertyValue("Content"));
+			Assert.IsTrue(checkedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(checkedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Checked Box and taking screenshot
 			checkedBox.Tap();
 			TakeScreenshot("After UnChecked");
-			Assert.IsFalse(checkedBox.GetDependencyPropertyValue<bool>("IsChecked"));			
+
+			//validation after clicking Checkbox 			
+			Assert.IsFalse(checkedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(checkedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
 		}
-		     
-        /* This function is to test the DisabledChecked option  in checkbox for material */       
+
+		/* This function is to test the DisabledChecked option  in checkbox for material */
 		[Test]
 		public void WhenMaterialClick_04_DisabledChecked()
 		{
+			//Navigation to Checkbox material control
 			NavigateToSample("CheckBox", "Material");
 
+			//Scrolling to Disabled Checked option
+			App.ScrollDownTo("Material_Disabled_Checked");
+
+			//Initial Validation
 			TakeScreenshot("Before UnChecked");
 			var materialDisabledCheckedBox = new QueryEx(x => x.All().Marked("Material_Disabled_Checked"));
-			App.ScrollDownTo("Material_Disabled_Checked");
+			Assert.AreEqual("DISABLED CHECKED", materialDisabledCheckedBox.GetDependencyPropertyValue("Content"));
+			Assert.IsTrue(materialDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsFalse(materialDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Unchecked Box and taking screenshot
 			materialDisabledCheckedBox.Tap();
 			TakeScreenshot("After UnChecked");
+
+			//validation after clicking Disabled Checkbox 						
+			Assert.IsTrue(materialDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
 			Assert.IsFalse(materialDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
-			Assert.IsTrue(materialDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));			
 		}
-		     
-        /* This function is to test the Indeterminate for uncheck, recheck and indeterminate option in checkbox for material   */     
+
+		/* This function is to test the Indeterminate for uncheck, recheck and indeterminate option in checkbox for material   */
 		[Test]
 		public void WhenMaterialClick_05_Indeterminate()
 		{
+			//Navigation to Checkbox material control
 			NavigateToSample("CheckBox", "Material");
 
+			//Scrolling to Indeterminate option
+			App.ScrollDownTo("Material_Indeterminate");
+
+			//Initial Validation
 			TakeScreenshot("Before UnChecked");
 			var checkedIndeterminateBox = new QueryEx(x => x.All().Marked("Material_Indeterminate"));
-			App.ScrollDownTo("Material_Indeterminate");
+			Assert.AreEqual("INDETERMINATE", checkedIndeterminateBox.GetDependencyPropertyValue("Content"));
+			Assert.IsTrue(checkedIndeterminateBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Indeterminate Box and taking screenshot
 			checkedIndeterminateBox.Tap();
 			TakeScreenshot("After UnChecked");
-			Assert.IsFalse(checkedIndeterminateBox.GetDependencyPropertyValue<bool>("IsChecked"));
 
+			//validation after clicking Indeterminate box.						
+			Assert.IsFalse(checkedIndeterminateBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(checkedIndeterminateBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on indeterminate Box second time and taking screenshot
 			checkedIndeterminateBox.Tap();
 			TakeScreenshot("After Checked");
+
+			//validation after clicking Indeterminate box.
 			Assert.IsTrue(checkedIndeterminateBox.GetDependencyPropertyValue<bool>("IsChecked"));
 
+			//Tap/Click on Indeterminate Box second time and taking screenshot.
 			checkedIndeterminateBox.Tap();
 			TakeScreenshot("After Checked");
+
+			//validation after clicking Indeterminate box.
 			var isChecked = checkedIndeterminateBox.GetDependencyPropertyValue("IsChecked");
-			Assert.IsTrue(isChecked == null || string.IsNullOrWhiteSpace(isChecked as string));			
+			Assert.IsTrue(isChecked == null || string.IsNullOrWhiteSpace(isChecked as string));
 		}
 
-		     
-        /* This function is to test the DisabledIndeterminate option  in checkbox for material.*/
-        
+		/* This function is to test the DisabledIndeterminate option  in checkbox for material.*/
+
 		[Test]
 		public void WhenMaterialClick_06_DisabledIndeterminate()
 		{
+			//Navigation to Checkbox material control
 			NavigateToSample("CheckBox", "Material");
 
+			//Scrolling to Disabled Indeterminate option
+			App.ScrollDownTo("Material_Disabled_Indeterminate");
+
+			//Initial Validation
 			TakeScreenshot("Before Checked");
 			var materialDisabledIndeterminateBox = new QueryEx(x => x.All().Marked("Material_Disabled_Indeterminate"));
-			App.ScrollDownTo("Material_Disabled_Indeterminate");
+			Assert.AreEqual("DISABLED INDETERMINATE", materialDisabledIndeterminateBox.GetDependencyPropertyValue("Content"));
+			Assert.IsFalse(materialDisabledIndeterminateBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Disabled Indeterminate Box and taking screenshot
 			materialDisabledIndeterminateBox.Tap();
 			TakeScreenshot("After Checked");
 
+			//validation after clicking Disabled Indeterminate box.
 			Assert.IsFalse(materialDisabledIndeterminateBox.GetDependencyPropertyValue<bool>("IsEnabled"));
 			var isChecked = materialDisabledIndeterminateBox.GetDependencyPropertyValue("IsChecked");
 			Assert.IsTrue(isChecked == null || string.IsNullOrWhiteSpace(isChecked as string));
+
 		}
 	}
 }
--- Uno.Gallery/Uno.Gallery.UITest/Given_CheckBox_03_Fluent.cs
+++ Uno.Gallery/Uno.Gallery.UITest/Given_CheckBox_03_Fluent.cs
@@ -17,103 +17,175 @@ namespace Uno.Gallery.UITests
 		[Test]
 		public void WhenFluentClick_01_Unchecked()
 		{
+			//Navigation to Checkbox Fluent control
 			NavigateToSample("CheckBox", "Fluent");
 
-			TakeScreenshot("Before Checked");			
+			//Scrolling to Unchecked option
+			App.ScrollDownTo("Fluent_Unchecked");
+
+			//Initial Validation
+			TakeScreenshot("Before Checked");
 			var FluentUncheckedBox = new QueryEx(x => x.All().Marked("Fluent_Unchecked"));
-			App.ScrollDownTo("Fluent_Unchecked");			
+			Assert.AreEqual("UNCHECKED", FluentUncheckedBox.GetDependencyPropertyValue("Content"));
+			Assert.IsFalse(FluentUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(FluentUncheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Unchecked Box and taking screenshot
 			FluentUncheckedBox.Tap();
 			TakeScreenshot("After Checked");
-			Assert.IsTrue(FluentUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));			
+
+			//validation after clicking Uncheckbox 
+			Assert.IsTrue(FluentUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(FluentUncheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
 		}
 
-		     
-        /* This function is to test the DisabledUnchecked option  in checkbox for Fluent. */        
+
+		/* This function is to test the DisabledUnchecked option  in checkbox for Fluent. */
 		[Test]
 		public void WhenFluentClick_02_DisabledUnchecked()
 		{
+			//Navigation to Checkbox Fluent control
 			NavigateToSample("CheckBox", "Fluent");
 
-			TakeScreenshot("Before Checked");			
-			var fluentDisabledUncheckedBox = new QueryEx(x => x.All().Marked("Fluent_Disabled_Unchecked"));
+			//Scrolling to Disabled Unchecked option
 			App.ScrollDownTo("Fluent_Disabled_Unchecked");
+
+			//Initial Validation
+			TakeScreenshot("Before Checked");
+			var fluentDisabledUncheckedBox = new QueryEx(x => x.All().Marked("Fluent_Disabled_Unchecked"));
+			Assert.AreEqual("DISABLED UNCHECKED", fluentDisabledUncheckedBox.GetDependencyPropertyValue("Content"));
+			Assert.IsFalse(fluentDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+			Assert.IsFalse(fluentDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+
+			//Tap/Click on Disabled Unchecked Box and taking screenshot
 			fluentDisabledUncheckedBox.Tap();
 			TakeScreenshot("After Checked");
+
+			//validation after clicking Uncheckbox		
 			Assert.IsFalse(fluentDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
-			Assert.IsFalse(fluentDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));			
+			Assert.IsFalse(fluentDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
 		}
 
-		     
-		 /* This function is to test the Checked option in checkbox for Fluent. */	 
+
+		/* This function is to test the Checked option in checkbox for Fluent. */
 		[Test]
 		public void WhenFluentClick_03_Checked()
 		{
+			//Navigation to Checkbox Fluent control
 			NavigateToSample("CheckBox", "Fluent");
 
-			TakeScreenshot("Before UnChecked");			
-			var fluentCheckedBox = new QueryEx(x => x.All().Marked("Fluent_Checked"));
+			//Scrolling to Checked option
 			App.ScrollDownTo("Fluent_Checked");
+
+			//Initial Validation
+			TakeScreenshot("Before UnChecked");
+			var fluentCheckedBox = new QueryEx(x => x.All().Marked("Fluent_Checked"));
+			Assert.AreEqual("CHECKED", fluentCheckedBox.GetDependencyPropertyValue("Content"));
+			Assert.IsTrue(fluentCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(fluentCheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Checked Box and taking screenshot
 			fluentCheckedBox.Tap();
 			TakeScreenshot("After UnChecked");
-			Assert.IsFalse(fluentCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));			
-		}		
-		     
-        /* This function is to test the DisabledChecked option  in checkbox for Fluent. */        
+
+			//validation after clicking Checkbox 			
+			Assert.IsFalse(fluentCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(fluentCheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+		}
+
+		/* This function is to test the DisabledChecked option  in checkbox for Fluent. */
 		[Test]
 		public void WhenFluentClick_04_DisabledChecked()
 		{
+			//Navigation to Checkbox Fluent control
 			NavigateToSample("CheckBox", "Fluent");
 
+			//Scrolling to Disabled Checked option
+			App.ScrollDownTo("Fluent_Disabled_Checked");
+
+			//Initial Validation
 			TakeScreenshot("Before Checked");
 			var fluentDisabledCheckedBox = new QueryEx(x => x.All().Marked("Fluent_Disabled_Checked"));
-			App.ScrollDownTo("Fluent_Disabled_Checked");
+			Assert.AreEqual("DISABLED CHECKED", fluentDisabledCheckedBox.GetDependencyPropertyValue("Content"));
+			Assert.IsTrue(fluentDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsFalse(fluentDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Disabled checked Box and taking screenshot
 			fluentDisabledCheckedBox.Tap();
 			TakeScreenshot("After UnChecked");
+
+			//validation after clicking Disabled checkbox 						
+			Assert.IsTrue(fluentDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
 			Assert.IsFalse(fluentDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
-			Assert.IsTrue(fluentDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));			
 		}
 
-		     
-        /* This function is to test the Indeterminate for uncheck, recheck and indeterminate option  in checkbox for Fluent. */        
+
+		/* This function is to test the Indeterminate for uncheck, recheck and indeterminate option  in checkbox for Fluent. */
 		[Test]
 		public void WhenFluentClick_05_Indeterminate()
 		{
+			//Navigation to Checkbox Fluent control
 			NavigateToSample("CheckBox", "Fluent");
 
+			//Scrolling to Indeterminate option
+			App.ScrollDownTo("Fluent_Indeterminate");
+
+			//Initial Validation
 			TakeScreenshot("Before UnChecked");
 			var fluentCheckedIndeterminateBox = new QueryEx(x => x.All().Marked("Fluent_Indeterminate"));
-			App.ScrollDownTo("Fluent_Indeterminate");
+			Assert.AreEqual("INDETERMINATE", fluentCheckedIndeterminateBox.GetDependencyPropertyValue("Content"));
+			Assert.IsTrue(fluentCheckedIndeterminateBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Indeterminate Box and taking screenshot
 			fluentCheckedIndeterminateBox.Tap();
 			TakeScreenshot("After UnChecked");
-			Assert.IsFalse(fluentCheckedIndeterminateBox.GetDependencyPropertyValue<bool>("IsChecked"));
 
+			//validation after clicking Indeterminate box.			
+			Assert.IsFalse(fluentCheckedIndeterminateBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(fluentCheckedIndeterminateBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on indeterminate Box second time and taking screenshot
 			fluentCheckedIndeterminateBox.Tap();
 			TakeScreenshot("After Checked");
+
+			//validation after clicking Indeterminate box.
 			Assert.IsTrue(fluentCheckedIndeterminateBox.GetDependencyPropertyValue<bool>("IsChecked"));
 
+			//Tap/Click on Indeterminate Box second time and taking screenshot.
 			fluentCheckedIndeterminateBox.Tap();
 			TakeScreenshot("After Checked");
+
+			//validation after clicking Indeterminate box.
 			var isChecked = fluentCheckedIndeterminateBox.GetDependencyPropertyValue("IsChecked");
-			Assert.IsTrue(isChecked == null || string.IsNullOrWhiteSpace(isChecked as string));			
-		}	
-		
-		     
-        /* This function is to test the DisabledIndeterminate option  in checkbox for Fluent. */        
+			Assert.IsTrue(isChecked == null || string.IsNullOrWhiteSpace(isChecked as string));
+		}
+
+
+		/* This function is to test the DisabledIndeterminate option  in checkbox for Fluent. */
 		[Test]
 		public void WhenFluentClick_06_DisabledIndeterminate()
 		{
+			//Navigation to Checkbox Fluent control
 			NavigateToSample("CheckBox", "Fluent");
 
-			TakeScreenshot("Before Checked");
-
-			var fluentDisabledIndeterminateBox = new QueryEx(x => x.All().Marked("Fluent_Disabled_Indeterminate"));
+			//Scrolling to Disabled Indeterminate option
 			App.ScrollDownTo("Fluent_Disabled_Indeterminate");
+
+			//Initial Validation
+			TakeScreenshot("Before Checked");
+			var fluentDisabledIndeterminateBox = new QueryEx(x => x.All().Marked("Fluent_Disabled_Indeterminate"));
+			Assert.AreEqual("DISABLED INDETERMINATE", fluentDisabledIndeterminateBox.GetDependencyPropertyValue("Content"));
+			Assert.IsFalse(fluentDisabledIndeterminateBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Disabled Indeterminate Box and taking screenshot
 			fluentDisabledIndeterminateBox.Tap();
-	
 			TakeScreenshot("After Checked");
+
+			//validation after clicking Disabled Indeterminate box.
 			Assert.IsFalse(fluentDisabledIndeterminateBox.GetDependencyPropertyValue<bool>("IsEnabled"));
 			var isChecked = fluentDisabledIndeterminateBox.GetDependencyPropertyValue("IsChecked");
-			Assert.IsTrue(isChecked == null || string.IsNullOrWhiteSpace(isChecked as string));			
+			Assert.IsTrue(isChecked == null || string.IsNullOrWhiteSpace(isChecked as string));
+
 		}
 	}
 }
