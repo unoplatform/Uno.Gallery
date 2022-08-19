557289dfc288027b68b4bc43cff54beac6a281a3
Sakshi Shukla <sakshi.shukla@nventive.com>
2022-08-19 14:26:47 -0400

    Ui Test: updated test script for checkbox controls(Material, Fluent, Cupertino)

--- Uno.Gallery/Uno.Gallery.UITest/Given_CheckBox_02_Material_Secondary.cs
+++ Uno.Gallery/Uno.Gallery.UITest/Given_CheckBox_02_Material_Secondary.cs
@@ -11,101 +11,175 @@ namespace Uno.Gallery.UITests
 {
 	[Ignore("WIP: M3 Migration")]
 	public class Given_CheckBox_02_Material_Secondary : TestBase
-	{		    
-        /*This function is to test the Unchecked option  in checkbox for material Secondary. */        
+	{
+		/*This function is to test the Unchecked option  in checkbox for material Secondary. */
 		[Test]
 		public void WhenMaterialClickSecondary_01_Unchecked()
 		{
+			//Navigation to Checkbox material control
 			NavigateToSample("CheckBox", "Material");
 
+			//Scrolling to Secondary Unchecked option
+			App.ScrollDownTo("Material_Secondary_Unchecked");
+
+			//Initial Validation
 			TakeScreenshot("Before Checked");
 			var materialSecondaryUncheckedBox = new QueryEx(x => x.All().Marked("Material_Secondary_Unchecked"));
-			App.ScrollDownTo("Material_Secondary_Unchecked");
+			Assert.AreEqual("UNCHECKED", materialSecondaryUncheckedBox.GetDependencyPropertyValue("Content"));
+			Assert.IsFalse(materialSecondaryUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(materialSecondaryUncheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Secondary Unchecked Box and taking screenshot
 			materialSecondaryUncheckedBox.Tap();
 			TakeScreenshot("After Checked");
-			Assert.IsTrue(materialSecondaryUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));			
+
+			//validation after clicking Secondary Uncheckbox 			
+			Assert.IsTrue(materialSecondaryUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(materialSecondaryUncheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
 		}
 
 		/* This function is to test the DisabledUnchecked option  in checkbox for material Secondary.*/
 		[Test]
 		public void WhenMaterialClickSecondary_02_DisabledUnchecked()
 		{
+			//Navigation to Checkbox material control
 			NavigateToSample("CheckBox", "Material");
 
+			//Scrolling to Secondary Disabled Unchecked option
+			App.ScrollDownTo("Material_Secondary_Disabled_Unchecked");
+
+			//Initial Validation
 			TakeScreenshot("Before Checked");
 			var materialSecondaryDisabledUncheckedBox = new QueryEx(x => x.All().Marked("Material_Secondary_Disabled_Unchecked"));
-			App.ScrollDownTo("Material_Secondary_Disabled_Unchecked");
+			Assert.AreEqual("DISABLED UNCHECKED", materialSecondaryDisabledUncheckedBox.GetDependencyPropertyValue("Content"));
+			Assert.IsFalse(materialSecondaryDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsFalse(materialSecondaryDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Secondary Disabled UncheckedBox and taking screenshot
 			materialSecondaryDisabledUncheckedBox.Tap();
 			TakeScreenshot("After Checked");
+
+			//validation after clicking Secondary Disabled Uncheckbox						
+			Assert.IsFalse(materialSecondaryDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
 			Assert.IsFalse(materialSecondaryDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
-			Assert.IsFalse(materialSecondaryDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));			
 		}
 
 		/* This function is to test the Checked option in checkbox for material Secondary. */
 		[Test]
 		public void WhenMaterialClickSecondary_03_Checked()
 		{
+			//Navigation to Checkbox material control
 			NavigateToSample("CheckBox", "Material");
 
-			TakeScreenshot("Before UnChecked");			
-			var materialSecondaryCheckedBox = new QueryEx(x => x.All().Marked("Material_Secondary_Checked"));
+			//Scrolling to Secondary Checked option
 			App.ScrollDownTo("Material_Secondary_Checked");
+
+			//Initial Validation
+			TakeScreenshot("Before UnChecked");
+			var materialSecondaryCheckedBox = new QueryEx(x => x.All().Marked("Material_Secondary_Checked"));
+			Assert.AreEqual("CHECKED", materialSecondaryCheckedBox.GetDependencyPropertyValue("Content"));
+			Assert.IsTrue(materialSecondaryCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(materialSecondaryCheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Secondary Checked Box and taking screenshot
 			materialSecondaryCheckedBox.Tap();
 			TakeScreenshot("After UnChecked");
-			Assert.IsFalse(materialSecondaryCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));		
+
+			//validation after clicking Secondary Checkbox 			
+			Assert.IsFalse(materialSecondaryCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(materialSecondaryCheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
 		}
 
 		/* This function is to test the DisabledChecked option  in checkbox for material Secondary. */
 		[Test]
 		public void WhenMaterialClickSecondary_04_DisabledChecked()
 		{
+			//Navigation to Checkbox material control
 			NavigateToSample("CheckBox", "Material");
 
+			//Scrolling to Disabled Checked option
+			App.ScrollDownTo("Material_Secondary_Disabled_Checked");
+
+			//Initial Validation
 			TakeScreenshot("Before Checked");
 			var materialSecondaryDisabledCheckedBox = new QueryEx(x => x.All().Marked("Material_Secondary_Disabled_Checked"));
-			App.ScrollDownTo("Material_Secondary_Disabled_Checked");
+			Assert.AreEqual("DISABLED CHECKED", materialSecondaryDisabledCheckedBox.GetDependencyPropertyValue("Content"));
+			Assert.IsFalse(materialSecondaryDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+			Assert.IsTrue(materialSecondaryDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+
+			//Tap/Click on Disabled Checked Box and taking screenshot
 			materialSecondaryDisabledCheckedBox.Tap();
 			TakeScreenshot("After UnChecked");
+
+			//validation after clicking Secondary Disabledcheckbox 
+			Assert.IsTrue(materialSecondaryDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
 			Assert.IsFalse(materialSecondaryDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
-			Assert.IsTrue(materialSecondaryDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));			
+
 		}
 
 		/* This function is to test the Indeterminate for uncheck, recheck and indeterminate option  in checkbox for material Secondary.*/
 		[Test]
 		public void WhenMaterialClickSecondary_05_Indeterminate()
 		{
+			//Navigation to Checkbox material control
 			NavigateToSample("CheckBox", "Material");
 
-			TakeScreenshot("Before UnChecked");			
-			var materialSecondaryCheckedIndeterminateBox = new QueryEx(x => x.All().Marked("Material_Secondary_Indeterminate"));
+			//Scrolling to Secondary Indeterminate option
 			App.ScrollDownTo("Material_Secondary_Indeterminate");
+
+			//Initial Validation
+			TakeScreenshot("Before UnChecked");
+			var materialSecondaryCheckedIndeterminateBox = new QueryEx(x => x.All().Marked("Material_Secondary_Indeterminate"));
+			Assert.AreEqual("INDETERMINATE", materialSecondaryCheckedIndeterminateBox.GetDependencyPropertyValue("Content"));
+			Assert.IsTrue(materialSecondaryCheckedIndeterminateBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Secondary Indeterminate Box and taking screenshot
 			materialSecondaryCheckedIndeterminateBox.Tap();
 			TakeScreenshot("After UnChecked");
-			Assert.IsFalse(materialSecondaryCheckedIndeterminateBox.GetDependencyPropertyValue<bool>("IsChecked"));
 
+			//validation after clicking Secondary Indeterminate box.
+			Assert.IsFalse(materialSecondaryCheckedIndeterminateBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(materialSecondaryCheckedIndeterminateBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Secondary Indeterminate Box second time and taking screenshot
 			materialSecondaryCheckedIndeterminateBox.Tap();
 			TakeScreenshot("After Checked");
+
+			//validation after clicking Secondary Indeterminate box.
 			Assert.IsTrue(materialSecondaryCheckedIndeterminateBox.GetDependencyPropertyValue<bool>("IsChecked"));
 
+			//Tap/Click on Secondary Indeterminate Box second time and taking screenshot.
 			materialSecondaryCheckedIndeterminateBox.Tap();
 			TakeScreenshot("After Checked");
+
+			//validation after clicking Secondary Indeterminate box.
 			var isChecked = materialSecondaryCheckedIndeterminateBox.GetDependencyPropertyValue("IsChecked");
-			Assert.IsTrue(isChecked == null || string.IsNullOrWhiteSpace(isChecked as string));			
+			Assert.IsTrue(isChecked == null || string.IsNullOrWhiteSpace(isChecked as string));
 		}
 
 		/* This function is to test the DisabledIndeterminate option  in checkbox for material Secondary */
 		[Test]
 		public void WhenMaterialClickSecondary_06_DisabledIndeterminate()
 		{
+			//Navigation to Checkbox material control
 			NavigateToSample("CheckBox", "Material");
 
-			TakeScreenshot("Before Checked");
-			var materialSecondaryDisabledIndeterminateBox = App.WaitThenTap("Material_Secondary_Disabled_Indeterminate").ToQueryEx();
+			//Scrolling to Secondary Disabled Indeterminate option
 			App.ScrollDownTo("Material_Secondary_Disabled_Indeterminate");
+
+			//Taking screenshot before clicking disabled Indeterminate checkbox.
+			TakeScreenshot("Before Checked");
+
+			//Tap/Click on Disabled Indeterminate Box and taking screenshot
+			var materialSecondaryDisabledIndeterminateBox = App.WaitThenTap("Material_Secondary_Disabled_Indeterminate").ToQueryEx();
 			TakeScreenshot("After Checked");
+
+			//validation after clicking Disabled Indeterminate box.
+			Assert.AreEqual("DISABLED INDETERMINATE", materialSecondaryDisabledIndeterminateBox.GetDependencyPropertyValue("Content"));
 			Assert.IsFalse(materialSecondaryDisabledIndeterminateBox.GetDependencyPropertyValue<bool>("IsEnabled"));
 			var isChecked = materialSecondaryDisabledIndeterminateBox.GetDependencyPropertyValue("IsChecked");
-			Assert.IsTrue(isChecked == null || string.IsNullOrWhiteSpace(isChecked as string));		
+			Assert.IsTrue(isChecked == null || string.IsNullOrWhiteSpace(isChecked as string));
+
 		}
 	}
 }
--- Uno.Gallery/Uno.Gallery.UITest/Given_CheckBox_04_Cupertino.cs
+++ Uno.Gallery/Uno.Gallery.UITest/Given_CheckBox_04_Cupertino.cs
@@ -9,7 +9,7 @@ using Uno.UITests.Helpers;
 
 namespace Uno.Gallery.UITests
 {
-	[Ignore("WIP: M3 Migration")]
+	//[Ignore("WIP: M3 Migration")]
 	public class Given_CheckBox_04_Cupertino : TestBase
 	{
 		     
@@ -17,102 +17,174 @@ namespace Uno.Gallery.UITests
 		[Test]
 		public void WhenCupertinoClick_01_Unchecked()
 		{
+			//Navigation to Checkbox Cupertino control
 			NavigateToSample("CheckBox", "Cupertino");
 
+			//Scrolling to Unchecked option
+			App.ScrollDownTo("Cupertino_Unchecked");
+
+			//Initial Validation
 			TakeScreenshot("Before Checked");
 			var cupertinoUncheckedBox = new QueryEx(x => x.Marked("Cupertino_Unchecked"));
-			App.ScrollDownTo("Cupertino_Unchecked");
+			Assert.AreEqual("UNCHECKED", cupertinoUncheckedBox.GetDependencyPropertyValue("Content"));
+			Assert.IsFalse(cupertinoUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(cupertinoUncheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Unchecked Box and taking screenshot
 			cupertinoUncheckedBox.Tap();
 			TakeScreenshot("After Checked");
-			Assert.IsTrue(cupertinoUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));			
+
+			//validation after clicking Uncheckbox 			
+			Assert.IsTrue(cupertinoUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(cupertinoUncheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
 		}
-		     
-        /* This function is to test the DisabledChecked option  in checkbox for Cupertino. */        
+
+		/* This function is to test the DisabledChecked option  in checkbox for Cupertino. */
 		[Test]
 		public void WhenCupertinoClick_02_DisabledUnChecked()
 		{
+			//Navigation to Checkbox Cupertino control
 			NavigateToSample("CheckBox", "Cupertino");
 
+			//Scrolling to Disabled Unchecked option
+			App.ScrollDownTo("Cupertino_Disabled_Unchecked");
+
+			//Initial Validation
 			TakeScreenshot("Before Checked");
 			var cupertinoDisabledUncheckedBox = new QueryEx(x => x.All().Marked("Cupertino_Disabled_Unchecked"));
-			App.ScrollDownTo("Cupertino_Disabled_Unchecked");
+			Assert.AreEqual("DISABLED UNCHECKED", cupertinoDisabledUncheckedBox.GetDependencyPropertyValue("Content"));
+			Assert.IsFalse(cupertinoDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsFalse(cupertinoDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Disabled Unchecked Box and taking screenshot
 			cupertinoDisabledUncheckedBox.Tap();
 			TakeScreenshot("After Checked");
+
+			//validation after clicking Uncheckbox						
+			Assert.IsFalse(cupertinoDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
 			Assert.IsFalse(cupertinoDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
-			Assert.IsFalse(cupertinoDisabledUncheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));			
 		}
-		     
-		 /* This function is to test the Checked option in checkbox for Cupertino.*/		 
+
+		/* This function is to test the Checked option in checkbox for Cupertino.*/
 		[Test]
 		public void WhenCupertinoClick_03_Checked()
 		{
+			//Navigation to Checkbox Cupertino control
 			NavigateToSample("CheckBox", "Cupertino");
 
+			//Scrolling to Checked option
+			App.ScrollDownTo("Cupertino_Checked");
 
+			//Initial Validation	
 			TakeScreenshot("Before UnChecked");
 			var cupertinoCheckedBox = new QueryEx(x => x.All().Marked("Cupertino_Checked"));
-			App.ScrollDownTo("Cupertino_Checked");
+			Assert.AreEqual("CHECKED", cupertinoCheckedBox.GetDependencyPropertyValue("Content"));
+			Assert.IsTrue(cupertinoCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(cupertinoCheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Checked Box and taking screenshot
 			cupertinoCheckedBox.Tap();
 			TakeScreenshot("After UnChecked");
-			Assert.IsFalse(cupertinoCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));			
+
+			//validation after clicking Checkbox			
+			Assert.IsFalse(cupertinoCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(cupertinoCheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
 		}
 
-		     
-        /* This function is to test the DisabledUnchecked option  in checkbox for Cupertino.*/        
+
+		/* This function is to test the DisabledUnchecked option  in checkbox for Cupertino.*/
 		[Test]
 		public void WhenCupertinoClick_04_DisabledChecked()
 		{
+			//Navigation to Checkbox Cupertino control
 			NavigateToSample("CheckBox", "Cupertino");
 
-			TakeScreenshot("Before Checked");
-			var cupertinoDisabledCheckedBox = new QueryEx(x => x.All().Marked("Cupertino_Disabled_Checked"));
+			//Scrolling to Disabled Checked option
 			App.ScrollDownTo("Cupertino_Disabled_Checked");
+
+			//Initial Validation
+			TakeScreenshot("Before UnChecked");
+			var cupertinoDisabledCheckedBox = new QueryEx(x => x.All().Marked("Cupertino_Disabled_Checked"));
+			Assert.AreEqual("DISABLED CHECKED", cupertinoDisabledCheckedBox.GetDependencyPropertyValue("Content"));
+			Assert.IsTrue(cupertinoDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsFalse(cupertinoDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Disabled checkedBox and taking screenshot
 			cupertinoDisabledCheckedBox.Tap();
 			TakeScreenshot("After UnChecked");
+
+			//validation after clicking on Disabled checkedBox 						
+			Assert.IsTrue(cupertinoDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));
 			Assert.IsFalse(cupertinoDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsEnabled"));
-			Assert.IsTrue(cupertinoDisabledCheckedBox.GetDependencyPropertyValue<bool>("IsChecked"));			
 		}
 
-		     
-        /* This function is to test the Indeterminate for uncheck, recheck and indeterminate option  in checkbox for Cupertino.*/        
+
+		/* This function is to test the Indeterminate for uncheck, recheck and indeterminate option  in checkbox for Cupertino.*/
 		[Test]
 		public void WhenCupertinoClick_05_Indeterminate()
 		{
+			//Navigation to Checkbox Cupertino control
 			NavigateToSample("CheckBox", "Cupertino");
 
+			//Scrolling to Indeterminate option
+			App.ScrollDownTo("Cupertino_Indeterminate");
 
+			//Initial Validation
 			TakeScreenshot("Before UnChecked");
 			var cupertinoCheckedIndeterminateBox = new QueryEx(x => x.All().Marked("Cupertino_Indeterminate"));
-			App.ScrollDownTo("Cupertino_Indeterminate");
+			Assert.AreEqual("INDETERMINATE", cupertinoCheckedIndeterminateBox.GetDependencyPropertyValue("Content"));
+			Assert.IsTrue(cupertinoCheckedIndeterminateBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Indeterminate Box and taking screenshot
 			cupertinoCheckedIndeterminateBox.Tap();
 			TakeScreenshot("After UnChecked");
-			Assert.IsFalse(cupertinoCheckedIndeterminateBox.GetDependencyPropertyValue<bool>("IsChecked"));
 
+			//validation after clicking Indeterminate box.			
+			Assert.IsFalse(cupertinoCheckedIndeterminateBox.GetDependencyPropertyValue<bool>("IsChecked"));
+			Assert.IsTrue(cupertinoCheckedIndeterminateBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on indeterminate Box second time and taking screenshot
 			cupertinoCheckedIndeterminateBox.Tap();
 			TakeScreenshot("After Checked");
+
+			//validation after clicking Indeterminate box.
 			Assert.IsTrue(cupertinoCheckedIndeterminateBox.GetDependencyPropertyValue<bool>("IsChecked"));
 
+			//Tap/Click on Indeterminate Box second time and taking screenshot.
 			cupertinoCheckedIndeterminateBox.Tap();
 			TakeScreenshot("After Checked");
+
+			//validation after clicking Indeterminate box.
 			var isChecked = cupertinoCheckedIndeterminateBox.GetDependencyPropertyValue("IsChecked");
-			Assert.IsTrue(isChecked == null || string.IsNullOrWhiteSpace(isChecked as string));			
+			Assert.IsTrue(isChecked == null || string.IsNullOrWhiteSpace(isChecked as string));
 		}
-		
-		     
-        /* This function is to test the DisabledIndeterminate option  in checkbox for Cupertino. */        
+
+
+		/* This function is to test the DisabledIndeterminate option  in checkbox for Cupertino. */
 		[Test]
 		public void WhenCupertinoClick_06_DisabledIndeterminate()
 		{
+			//Navigation to Checkbox Cupertino control
 			NavigateToSample("CheckBox", "Cupertino");
 
+			//Scrolling to Disabled Indeterminate option
+			App.ScrollDownTo("Cupertino_Disabled_Indeterminate");
+
+			//Initial Validation
 			TakeScreenshot("Before Checked");
 			var cupertinoDisabledIndeterminateBox = new QueryEx(x => x.All().Marked("Cupertino_Disabled_Indeterminate"));
-			App.ScrollDownTo("Cupertino_Disabled_Indeterminate");
+			Assert.AreEqual("DISABLED INDETERMINATE", cupertinoDisabledIndeterminateBox.GetDependencyPropertyValue("Content"));
+			Assert.IsFalse(cupertinoDisabledIndeterminateBox.GetDependencyPropertyValue<bool>("IsEnabled"));
+
+			//Tap/Click on Disabled Indeterminate Box and taking screenshot
 			cupertinoDisabledIndeterminateBox.Tap();
 			TakeScreenshot("After Checked");
+
+			//validation after clicking Disabled Indeterminate box.
 			Assert.IsFalse(cupertinoDisabledIndeterminateBox.GetDependencyPropertyValue<bool>("IsEnabled"));
 			var isChecked = cupertinoDisabledIndeterminateBox.GetDependencyPropertyValue("IsChecked");
-			Assert.IsTrue(isChecked == null || string.IsNullOrWhiteSpace(isChecked as string));			
+			Assert.IsTrue(isChecked == null || string.IsNullOrWhiteSpace(isChecked as string));
+
 		}
 	}
 }
