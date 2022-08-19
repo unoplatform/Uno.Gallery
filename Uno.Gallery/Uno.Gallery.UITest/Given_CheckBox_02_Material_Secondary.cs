526e4189af1964abe41f685a05c2fb370033a870
Sakshi Shukla <sakshi.shukla@nventive.com>
2022-08-18 22:46:13 -0400

    test: New UI automation script for Card control

--- Uno.Gallery/Uno.Gallery.UITest/Given_CheckBox_02_Material_Secondary.cs
+++ Uno.Gallery/Uno.Gallery.UITest/Given_CheckBox_02_Material_Secondary.cs
@@ -9,7 +9,7 @@ using Uno.UITests.Helpers;
 
 namespace Uno.Gallery.UITests
 {
-	[Ignore("WIP: M3 Migration")]
+	//[Ignore("WIP: M3 Migration")]
 	public class Given_CheckBox_02_Material_Secondary : TestBase
 	{		    
         /*This function is to test the Unchecked option  in checkbox for material Secondary. */        
