dc43499bcc7d62371641070a02a4e8cf304381ad
TopperDEL <github_com@tparth.de>
2022-10-17 14:18:21 +0200

    fix: Disable for UWP

--- Uno.Gallery/Uno.Gallery.UWP/Views/SamplePages/VibrationDeviceSamplePage.xaml.cs
+++ Uno.Gallery/Uno.Gallery.UWP/Views/SamplePages/VibrationDeviceSamplePage.xaml.cs
@@ -7,7 +7,10 @@ using Windows.Phone.Devices.Notification;
 namespace Uno.Gallery.Views.Samples
 {
 	[SamplePage(SampleCategory.NonUIFeatures, "Vibration", Description = "Activates the vibration device for a certain amount of time.", DocumentationLink = "https://learn.microsoft.com/en-us/uwp/api/windows.devices.haptics.vibrationdevice")]
-    public sealed partial class VibrationDeviceSamplePage : Page
+#if !HAS_UNO
+	[SampleConditional(SampleConditionals.Disabled, Reason = "API is not supported")]
+#endif
+	public sealed partial class VibrationDeviceSamplePage : Page
 	{
         public VibrationDeviceSamplePage()
 		{
@@ -16,20 +19,24 @@ namespace Uno.Gallery.Views.Samples
 
 		private void Vibrate100_Click(object sender, RoutedEventArgs args)
 		{
+#if HAS_UNO
 			var vibrationDevice = VibrationDevice.GetDefault();
 			if (vibrationDevice != null)
 			{
 				vibrationDevice.Vibrate(TimeSpan.FromMilliseconds(1000));
 			}
+#endif
 		}
 
 		private void Vibrate1000_Click(object sender, RoutedEventArgs args)
 		{
+#if HAS_UNO
 			var vibrationDevice = VibrationDevice.GetDefault();
 			if (vibrationDevice != null)
 			{
 				vibrationDevice.Vibrate(TimeSpan.FromMilliseconds(1000));
 			}
+#endif
 		}
 	}
 }
