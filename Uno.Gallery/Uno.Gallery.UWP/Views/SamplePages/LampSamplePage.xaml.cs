0c14ddb09bfe740612d1b571927aedb792525cb8
Marcin Badurowicz <m@badurowicz.net>
2022-10-25 22:05:41 +0200

    Add Lamp usage sample

---
+++ Uno.Gallery/Uno.Gallery.UWP/Views/SamplePages/LampSamplePage.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Threading.Tasks;
+using Uno.Gallery.ViewModels;
+using Windows.Devices.Lights;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.Gallery.Views.Samples
+{
+	[SamplePage(SampleCategory.NonUIFeatures, "Lamp", Description = "Represents a lamp device. Allows you to turn the phone's camera flashlight on and off.", DocumentationLink = "https://learn.microsoft.com/en-us/uwp/api/Windows.Devices.Lights.Lamp")]
+	public sealed partial class LampSamplePage : Page
+	{
+		public LampSamplePage()
+		{
+			this.InitializeComponent();
+		}
+
+		private void ChangeLampState_Click(object sender, RoutedEventArgs e)
+		{
+			if ((sender as Button)?.DataContext is LampSamplePageViewModel viewModel)
+			{
+				if (!viewModel.IsEnabled)
+				{
+					viewModel.Enable();
+				}
+				else
+				{
+					viewModel.Disable();
+				}
+			}
+		}
+	}
+
+	public class LampSamplePageViewModel : ViewModelBase, IDisposable
+	{
+		private const string _enable = "Enable flashlight";
+		private const string _disable = "Disable flashlight";
+		private const string _unavailable = "Flashlight is not available on this device or platform";
+		private Lamp _lamp;
+
+		public string ButtonContent
+		{
+			get => GetProperty<string>();
+			set => SetProperty<string>(value);
+		}
+
+		public bool IsEnabled
+		{
+			get => GetProperty<bool>();
+			set => SetProperty<bool>(value);
+		}
+
+		public bool IsBrightnessSupported
+		{
+			get => GetProperty<bool>();
+			set => SetProperty<bool>(value);
+		}
+
+		public bool IsAvailable
+		{
+			get => GetProperty<bool>();
+			set => SetProperty<bool>(value);
+		}
+
+		public float BrightnessLevel
+		{
+			get => GetProperty<float>();
+			set => SetProperty<float>(value);
+		}
+
+		public LampSamplePageViewModel()
+		{
+			ButtonContent = _enable;
+			IsAvailable = true;
+			IsBrightnessSupported = false;
+			BrightnessLevel = 1;
+#if __IOS__
+IsBrightnessSupported = true;
+#endif
+			Task.Run(async () =>
+			{
+				_lamp = await Lamp.GetDefaultAsync();
+				IsAvailable = _lamp != null;
+				if (!IsAvailable)
+					ButtonContent = _unavailable;
+			});
+		}
+
+		public async void Enable()
+		{
+			if (_lamp != null)
+			{
+				ButtonContent = _disable;
+				_lamp.BrightnessLevel = BrightnessLevel;
+				_lamp.IsEnabled = true;
+				IsEnabled = true;
+			}
+			else
+			{
+				IsAvailable = false;
+				ButtonContent = _unavailable;
+			}
+		}
+
+		public void Disable()
+		{
+			if (_lamp != null)
+			{
+				_lamp.IsEnabled = false;
+				IsEnabled = false;
+				ButtonContent = _enable;
+			}
+		}
+
+		public void Dispose()
+		{
+			if (_lamp != null)
+				_lamp.Dispose();
+		}
+	}
+}
