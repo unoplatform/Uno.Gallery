3cb780769fd77f5f8355a347d3a67ed95ea75862
Dominik Titl <78549750+morning4coffe-dev@users.noreply.github.com>
2023-03-18 18:07:28 +0100

    fix: InfoBadge sample design improvements

--- Uno.Gallery/Uno.Gallery.UWP/Views/SamplePages/InfoBadgeSamplePage.xaml.cs
+++ Uno.Gallery/Uno.Gallery.UWP/Views/SamplePages/InfoBadgeSamplePage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using Uno.Gallery.ViewModels;
-using Windows.Devices.Sensors;
-using Windows.UI;
+﻿using Uno.Gallery.ViewModels;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -25,7 +20,9 @@ namespace Uno.Gallery.Views.Samples
 	{
 		public bool AreBadgesVisible { get => GetProperty<bool>(); set => SetProperty(value); }
 		public int NumericValue { get => GetProperty<int>(); set => SetProperty(value); }
-		public string SelectedStyle { get => GetProperty<string>(); set
+		public string SelectedStyle
+		{
+			get => GetProperty<string>(); set
 			{
 				SetProperty(value);
 				BadgeStyle = Application.Current.Resources["InfoBadgeStyle_" + SelectedStyle] as Style;
@@ -33,20 +30,12 @@ namespace Uno.Gallery.Views.Samples
 		}
 		public double Opacity { get => GetProperty<double>(); set => SetProperty(value); }
 		public Style BadgeStyle { get => GetProperty<Style>(); set => SetProperty(value); }
-		public NavigationViewPaneDisplayMode PaneDisplayMode { get => GetProperty<NavigationViewPaneDisplayMode>(); set => SetProperty(value); }
-		public List<NavigationViewPaneDisplayMode> AvailablePaneDisplayModes = new List<NavigationViewPaneDisplayMode>() {
-			NavigationViewPaneDisplayMode.Auto,
-			NavigationViewPaneDisplayMode.Left,
-			NavigationViewPaneDisplayMode.LeftCompact,
-			NavigationViewPaneDisplayMode.LeftMinimal,
-			NavigationViewPaneDisplayMode.Top
-		};
 
 		public InfoBadgeSamplePageViewModel()
 		{
-			PaneDisplayMode = NavigationViewPaneDisplayMode.Auto;
 			AreBadgesVisible = true;
 			NumericValue = 1;
+			SelectedStyle = "None";
 			BadgeStyle = Application.Current.Resources["InfoBadgeStyle_None"] as Style;
 			Opacity = 1;
 		}
