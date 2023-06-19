87069e427b74de6ea85dda0dadf87d4b41abd2e9
Jerome Laban <jerome.laban@nventive.com>
2023-06-19 11:26:15 -0400

    ci: Adjust UI tests

--- build/scripts/android-uitest-run.sh
+++ build/scripts/android-uitest-run.sh
@@ -6,10 +6,8 @@ export UNO_UITEST_SCREENSHOT_PATH=$BUILD_ARTIFACTSTAGINGDIRECTORY/screenshots/an
 export UNO_UITEST_PLATFORM=Android
 export UNO_UITEST_ANDROIDAPK_PATH=$BUILD_SOURCESDIRECTORY/Uno.Gallery/Uno.Gallery.Droid/bin/Release/com.nventive.uno.ui.demo-Signed.apk
 export UNO_UITEST_PROJECT=$BUILD_SOURCESDIRECTORY/Uno.Gallery/Uno.Gallery.UITest/Uno.Gallery.UITest.csproj
-export UNO_UITEST_ANDROID_PROJECT=$BUILD_SOURCESDIRECTORY/Uno.Gallery/Uno.Gallery.Droid/Uno.Gallery.Droid.csproj
+export UNO_UITEST_ANDROID_PROJECT=$BUILD_SOURCESDIRECTORY/Uno.Gallery/Uno.Gallery.Mobile
 export UNO_UITEST_BINARY=$BUILD_SOURCESDIRECTORY/Uno.Gallery/Uno.Gallery.UITest/bin/Release/net47/Uno.Gallery.UITest.dll
-export UNO_UITEST_NUNIT_VERSION=3.11.1
-export UNO_UITEST_NUGET_URL=https://dist.nuget.org/win-x86-commandline/v5.7.0/nuget.exe
 export UNO_EMULATOR_INSTALLED=$BUILD_SOURCESDIRECTORY/build/.emulator_started
 
 if [ ! -f "$UNO_EMULATOR_INSTALLED" ];
@@ -30,9 +28,9 @@ then
 	touch "$UNO_EMULATOR_INSTALLED"
 fi
 
-# build the sample, while the emulator is starting
-msbuild /r /p:Configuration=Release $UNO_UITEST_PROJECT
-msbuild /r /p:Configuration=Release /p:IsUiAutomationMappingEnabled=True /p:AndroidUseSharedRuntime=false /p:AotAssemblies=false /t:SignAndroidPackage $UNO_UITEST_ANDROID_PROJECT
+# Build the sample, while the emulator is starting
+cd $UNO_UITEST_ANDROID_PROJECT
+dotnet publish -f net7.0-android -c Release -p:RuntimeIdentifier=android-x64 /p:IsUiAutomationMappingEnabled=True /p:AndroidUseSharedRuntime=false /p:AotAssemblies=false
 
 mkdir -p $UNO_UITEST_SCREENSHOT_PATH
 
@@ -45,11 +43,12 @@ $ANDROID_HOME/platform-tools/adb devices
 
 echo "Emulator started"
 
-cd $BUILD_SOURCESDIRECTORY/build
+cd $UNO_UITEST_PROJECT
 
-wget $UNO_UITEST_NUGET_URL
-mono nuget.exe install NUnit.ConsoleRunner -Version $UNO_UITEST_NUNIT_VERSION
-
-mono $BUILD_SOURCESDIRECTORY/build/NUnit.ConsoleRunner.$UNO_UITEST_NUNIT_VERSION/tools/nunit3-console.exe \
-	$UNO_UITEST_BINARY \
-	--timeout=2700000
+dotnet test \
+	-c Release \
+	-l:"console;verbosity=normal" \
+	--logger "nunit;LogFileName=$BUILD_SOURCESDIRECTORY/build/TestResult.xml" \
+	--blame-hang-timeout $UITEST_TEST_TIMEOUT \
+	-v m \
+	|| true
