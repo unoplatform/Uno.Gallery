9a13371fd7b57e5b9139d1da07b82b8b3bd77d10
Jerome Laban <jerome.laban@nventive.com>
2023-06-20 11:00:27 -0400

    ci: Adjust logging in release

--- Uno.Gallery/Uno.Gallery.UWP/App.xaml.cs
+++ Uno.Gallery/Uno.Gallery.UWP/App.xaml.cs
@@ -343,7 +343,7 @@ namespace Uno.Gallery
 		/// </summary>
 		private static void InitializeLogging()
 		{
-#if DEBUG || __IOS__
+#if true // Force enable logging for debugging CI // DEBUG || __IOS__
 			// Logging is disabled by default for release builds, as it incurs a significant
 			// initialization cost from Microsoft.Extensions.Logging setup. If startup performance
 			// is a concern for your application, keep this disabled. If you're running on web or 
