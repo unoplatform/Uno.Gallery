ce7a574915dc73d97b62ef65643d7a8426724fad
xiaoy312 <xiaoyao312@gmail.com>
2023-08-09 18:10:00 -0400

    chore: fix build

---
+++ Uno.Gallery/Uno.Gallery.Shared/Entities/Data/TreeItem.cs
@@ -0,0 +1,61 @@
+﻿﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Uno.Gallery.Entities.Data
+{
+	public class TreeItem
+	{
+		public TreeItem(string name)
+		{
+			Name = name;
+		}
+
+		public string Name { get; }
+		public string Glyph => Children.Count > 0 ? "\uE8B7" : "\uE8A5";
+		public ObservableCollection<TreeItem> Children { get; set; } = new ObservableCollection<TreeItem>();
+	}
+
+	public class TreeItemCollection : ObservableCollection<TreeItem>
+	{
+		public TreeItemCollection() : base(GetTreeItem())
+		{
+		}
+
+		private static IEnumerable<TreeItem> GetTreeItem()
+		{
+			yield return new TreeItem("Documents")
+			{
+				Children = new ObservableCollection<TreeItem>
+				{
+					new TreeItem("Personal"),
+					new TreeItem("Projects"),
+					new TreeItem("Work")
+				}
+			};
+
+			yield return new TreeItem("Pictures")
+			{
+				Children = new ObservableCollection<TreeItem>
+				{
+					new TreeItem("2022")
+					{
+						Children = new ObservableCollection<TreeItem>
+						{
+							new TreeItem("Spring"),
+							new TreeItem("Winter")
+						}
+					},
+					new TreeItem("2023")
+					{
+						Children = new ObservableCollection<TreeItem>
+						{
+							new TreeItem("May"),
+							new TreeItem("July"),
+							new TreeItem("September"),
+						}
+					},
+				}
+			};
+		}
+	}
+}
