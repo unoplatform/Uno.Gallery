b81b7363cd3fa8aa2764f339cd967bd884aadfe9
Marc-Antoine Soucy <ma.soucy@nventive.com>
2023-08-10 11:11:00 -0400

    feat: add lightweight extension

---
+++ Uno.Gallery/Uno.Gallery.Shared/Extensions/ResourceExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+
+namespace Uno.Gallery
+{
+	/// <summary>
+	/// Helper class for Resources Extensions.
+	/// </summary>
+	public static class ResourceExtensions
+	{
+		public static readonly DependencyProperty OverridePathProperty =
+		DependencyProperty.RegisterAttached(
+			"OverridePath",
+			typeof(string),
+			typeof(ResourceExtensions),
+			new PropertyMetadata(null, OnOverridePathChanged));
+
+		public static void SetOverridePath(FrameworkElement element, string value)
+		{
+			element.SetValue(OverridePathProperty, value);
+		}
+
+		public static string GetOverridePath(FrameworkElement element)
+		{
+			return (string)element.GetValue(OverridePathProperty);
+		}
+
+		private static void OnOverridePathChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			if (d is FrameworkElement fe)
+			{
+				fe.Resources = e.NewValue is string path
+					? new ResourceDictionary() { Source = new Uri(path, UriKind.RelativeOrAbsolute) }
+					: default;
+			}
+		}
+	}
+}
---
+++ Uno.Gallery/Uno.Gallery.Shared/Views/SamplePages/LightWeightExtensionSameplePage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace Uno.Gallery.Views.SamplePages
+{
+	[SamplePage(SampleCategory.Theming, "Lightweight Extension", Description = "Using an extension with dictionaries centralizes key overrides, promoting reusability and consistency across controls, reducing the need for repetitive per-component customizations.", DocumentationLink = "https://learn.microsoft.com/en-us/windows/apps/design/style/xaml-styles#lightweight-styling")]
+	public sealed partial class LightWeightExtensionSameplePage : Page
+	{
+		public LightWeightExtensionSameplePage()
+		{
+			this.InitializeComponent();
+		}
+	}
+}
