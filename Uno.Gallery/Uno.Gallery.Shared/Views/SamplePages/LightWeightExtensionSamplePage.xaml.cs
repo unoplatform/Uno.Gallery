46c2849c4a8072aa07cefb16145c4a1cd3a1bbbf
Marc-Antoine Soucy <ma.soucy@nventive.com>
2023-08-15 08:34:07 -0400

    fix: sample page name

--- Uno.Gallery/Uno.Gallery.Shared/Views/SamplePages/LightWeightExtensionSamplePage.xaml.cs
+++ Uno.Gallery/Uno.Gallery.Shared/Views/SamplePages/LightWeightExtensionSamplePage.xaml.cs
@@ -3,9 +3,9 @@
 namespace Uno.Gallery.Views.SamplePages
 {
 	[SamplePage(SampleCategory.Theming, "Lightweight Extension", Description = "Using an extension with dictionaries centralizes key overrides, promoting reusability and consistency across controls, reducing the need for repetitive per-component customizations.", DocumentationLink = "https://learn.microsoft.com/en-us/windows/apps/design/style/xaml-styles#lightweight-styling")]
-	public sealed partial class LightWeightExtensionSameplePage : Page
+	public sealed partial class LightWeightExtensionSamplePage : Page
 	{
-		public LightWeightExtensionSameplePage()
+		public LightWeightExtensionSamplePage()
 		{
 			this.InitializeComponent();
 		}
