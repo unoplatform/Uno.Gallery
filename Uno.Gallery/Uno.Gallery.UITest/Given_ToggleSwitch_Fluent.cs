b655006b8c2f013acf31f19289902ef37b9bbac0
Darsh0307 <pateldarsh1015@gmail.com>
2024-02-01 11:57:43 -0500

    feat: toggle Switch content for fluent

---
+++ Uno.Gallery/Uno.Gallery.UITest/Given_ToggleSwitch_Fluent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Uno.UITest.Helpers.Queries;
+using Uno.UITests.Helpers;
+
+namespace Uno.Gallery.UITests
+{
+
+	public class Given_ToggleSwitch_Fluent : TestBase
+	{
+
+		[Test]
+		public void WhenToggleSwitchOff()
+		{
+			NavigateToSample("ToggleSwitch","Fluent");
+
+			TakeScreenshot("Before Click");
+		}
+
+	}
+}
