56a47456fdf633a6ba1f0ed21e56f5ad7f5a6c7f
Darsh0307 <pateldarsh1015@gmail.com>
2024-02-05 22:24:56 -0500

    feat:  added the no header passwordbox

---
+++ Uno.Gallery/Uno.Gallery.UITest/Given_PasswordBox_Fluent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Uno.UITest.Helpers.Queries;
+using Uno.UITests.Helpers;
+
+namespace Uno.Gallery.UITests
+{
+	//[Ignore("WIP: M3 Migration")]
+	public class Given_PasswordBox_Fluent : TestBase
+	{
+
+		[Test]
+		public void WhenPasswordBoxNoHeaer() 
+		{
+			NavigateToSample("PasswordBox", "Fluent");
+
+			TakeScreenshot("Before PasswordBox");
+
+			var NoPasswordBox = new QueryEx(x => x.All().Marked("PasswordBox_NoHeader"));
+			App.ScrollDownTo("PasswordBox_NoHeader");
+			NoPasswordBox.EnterText("Uno platform");
+
+			TakeScreenshot("After PasswordBox");
+			Assert.IsTrue(NoPasswordBox.GetDependencyPropertyValue<bool>(""));
+
+
+		}
+	}
+}
