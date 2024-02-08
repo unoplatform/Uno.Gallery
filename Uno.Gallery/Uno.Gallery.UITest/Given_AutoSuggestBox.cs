7438a82416a15586a8eeca337c5f273ae7e2fec8
Darsh0307 <pateldarsh1015@gmail.com>
2024-02-08 15:51:13 -0500

    feat: introducing UI test for AutoSuggestBox

---
+++ Uno.Gallery/Uno.Gallery.UITest/Given_AutoSuggestBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Uno.UITest.Helpers.Queries;
+using Uno.UITests.Helpers;
+
+
+namespace Uno.Gallery.UITests
+{
+	public class Given_AutoSuggestBox : TestBase
+	{
+		[Test]
+		public void AutoSuggestBox_Default()
+		{
+			NavigateToSample("AutoSuggestBox", "Fluent");
+			TakeScreenshot("Before Text");
+
+		}
+
+	}
+}
