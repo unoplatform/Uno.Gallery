3ca02d50b62735db2438428685c110fc5c1c3470
Darsh0307 <pateldarsh1015@gmail.com>
2024-02-13 15:04:54 -0500

    feat: creating UI test for rating control

---
+++ Uno.Gallery/Uno.Gallery.UITest/Given_RatingControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.UI.Xaml;
+using NUnit.Framework;
+using Uno.UITest.Helpers;
+using Uno.UITest.Helpers.Queries;
+using Uno.UITests.Helpers;
+
+namespace Uno.Gallery.UITests
+{
+	public class Given_RatingControl : TestBase
+	{
+		[Test]
+		public void RatingControl()
+		{
+			NavigateToSample("RatingControl", "Fluent");
+			var Rating = new QueryEx(x => x.All().Marked("Rating_Fluent"));
+			var Minus = new QueryEx(x => x.All().Marked("Minus_Fluent"));
+			var value = new QueryEx(x => x.All().Marked("Value_Fluent"));
+			Minus.Tap();
+			
+		}
+	}
+}
