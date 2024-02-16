cb3231898cbfdb41fb27512790aa8f56919acf4c
Darsh0307 <pateldarsh1015@gmail.com>
2024-02-16 12:56:08 -0500

    feat: ui test for plus and clear

--- Uno.Gallery/Uno.Gallery.UITest/Given_RatingControl.cs
+++ Uno.Gallery/Uno.Gallery.UITest/Given_RatingControl.cs
@@ -20,8 +20,22 @@ namespace Uno.Gallery.UITests
 			var Rating = new QueryEx(x => x.All().Marked("Rating_Fluent"));
 			var Minus = new QueryEx(x => x.All().Marked("Minus_Fluent"));
 			var value = new QueryEx(x => x.All().Marked("Value_Fluent"));
+			var Plus = new QueryEx(x => x.All().Marked("Plus_Fluent"));
+
+
 			Minus.Tap();
-			
+			Assert.AreEqual("Value:  2", value.GetDependencyPropertyValue<string>("Text"));
+			var clear = new QueryEx(x => x.All().Marked("Clear_Fluent"));
+			Plus.Tap();
+			Assert.AreEqual("Value:  2", value.GetDependencyPropertyValue<string>("Text"));
+			Minus.Tap();
+			Assert.AreEqual("Value:  1", value.GetDependencyPropertyValue<string>("Text"));
+			Minus.Tap();
+			Assert.AreEqual("Value:  3", value.GetDependencyPropertyValue<string>("Text"));
+
+
+
+
 		}
 	}
 }
