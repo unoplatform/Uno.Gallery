a66056327ba315f75838782db3676b9f792559eb
Darsh0307 <pateldarsh1015@gmail.com>
2024-02-18 22:36:32 -0500

    feat: ui test for rating control

--- Uno.Gallery/Uno.Gallery.UITest/Given_RatingControl.cs
+++ Uno.Gallery/Uno.Gallery.UITest/Given_RatingControl.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using NUnit.Framework;
+using Uno.UI.Xaml;
 using Uno.UITest.Helpers;
 using Uno.UITest.Helpers.Queries;
 using Uno.UITests.Helpers;
@@ -21,7 +23,14 @@ namespace Uno.Gallery.UITests
 			var Minus = new QueryEx(x => x.All().Marked("Minus_Fluent"));
 			var Value = new QueryEx(x => x.All().Marked("Value_Fluent"));
 			var Plus = new QueryEx(x => x.All().Marked("Plus_Fluent"));
+			var slider = new QueryEx(x => x.All().Marked("Slider_Fluent"));
+			var Read = new QueryEx(x => x.All().Marked("ReadOnly_Fluent"));
+			var Drag = new QueryEx(x => x.All().Marked("CanDrag_Fluent"));
+			var Clear = new QueryEx(x => x.All().Marked("CLearOnly_Fluent"));
+			var Caption = new QueryEx(x => x.All().Marked("Caption_Fluent"));
+			var Rate = new QueryEx(x => x.All().Marked("Rating_Control"));
 
+			
 
 			Minus.Tap();
 			Assert.AreEqual("Value:  2", Value.GetDependencyPropertyValue<string>("Text"));
@@ -31,8 +40,24 @@ namespace Uno.Gallery.UITests
 			Plus.Tap();
 			Assert.AreEqual("Value:  4", Value.GetDependencyPropertyValue<string>("Text"));
 			Plus.Tap();
+			Assert.AreEqual("Value:  5", Value.GetDependencyPropertyValue<string>("Text"));
+			Minus.Tap();
+			Assert.AreEqual("Value:  4", Value.GetDependencyPropertyValue<string>("Text"));
+			Assert.AreEqual("4", Rate.GetDependencyPropertyValue<string>("Value"));
+			Plus.Tap();
+			Assert.AreEqual("Value:  5", Value.GetDependencyPropertyValue<string>("Text"));
+			Assert.AreEqual("5", Rate.GetDependencyPropertyValue<string>("Value"));
 			RatingControl_Clear.Tap();
 			Assert.AreEqual("Value:  1", Value.GetDependencyPropertyValue<string>("Text"));
+			Read.Tap();
+			Assert.IsTrue(Read.GetDependencyPropertyValue<bool>("IsChecked"));
+			Drag.Tap();
+			Assert.IsTrue(Drag.GetDependencyPropertyValue<bool>("IsChecked"));
+			Clear.Tap();
+			Assert.IsTrue(Clear.GetDependencyPropertyValue<bool>("IsChecked"));
+			Caption.EnterText("Darsh");
+			Clear.Tap();
+			Assert.AreEqual("Darsh", Rate.GetDependencyPropertyValue<string>("Caption"));
 
 
 
