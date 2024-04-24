d713ce08b8e1c9a2cc4b48d7108509279c6a7688
Vatsa Shah <vatsashah45@gmail.com>
2024-04-24 11:41:28 -0400

    feat: SingleProject - Gallery

---
+++ Uno.Gallery/Platforms/Android/MainActivity.Android.cs
@@ -0,0 +1,17 @@
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content.PM;
+using Android.Views;
+
+namespace Uno.Gallery
+{
+	[Activity(
+			MainLauncher = true,
+			ConfigurationChanges = global::Uno.UI.ActivityHelper.AllConfigChanges,
+			WindowSoftInputMode = SoftInput.AdjustPan | SoftInput.StateHidden
+		)]
+	public class MainActivity : Microsoft.UI.Xaml.ApplicationActivity
+	{
+	}
+}
