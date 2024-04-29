7570d831e03956e398446269d55689c6b43e994a
agneszitte <agnes@platform.uno>
2024-04-29 18:35:33 -0400

    test adjustments to be able to build and test android on the emulator

--- Uno.Gallery/Uno.Gallery.Shared/App.xaml.cs
+++ Uno.Gallery/Uno.Gallery.Shared/App.xaml.cs
@@ -81,7 +81,7 @@ namespace Uno.Gallery
 			{
 #if __IOS__ && USE_UITESTS
 				// requires Xamarin Test Cloud Agent
-				Xamarin.Calabash.Start();
+				//Xamarin.Calabash.Start();
 #endif
 
 				MainWindow.Content = _shell = BuildShell();
