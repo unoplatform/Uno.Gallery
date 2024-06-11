d1a9737c5fe786227d2d2689c5a0d8aeca7b558d
Jerome Laban <jerome@platform.uno>
2024-06-11 10:07:18 -0400

    chore: Move to 17.2 emulator

--- build/scripts/ios-uitest-run.sh
+++ build/scripts/ios-uitest-run.sh
@@ -10,7 +10,7 @@ export UNO_UITEST_LOGFILE=$BUILD_ARTIFACTSTAGINGDIRECTORY/screenshots/ios/nunit-
 export UNO_UITEST_IOS_PROJECT=$BUILD_SOURCESDIRECTORY/Uno.Gallery
 export UITEST_TEST_TIMEOUT=60m
 
-export UNO_UITEST_SIMULATOR_VERSION="com.apple.CoreSimulator.SimRuntime.iOS-16-1"
+export UNO_UITEST_SIMULATOR_VERSION="com.apple.CoreSimulator.SimRuntime.iOS-17-2"
 export UNO_UITEST_SIMULATOR_NAME="iPad Pro (12.9-inch) (6th generation)"
 
 echo "Lising iOS simulators"
