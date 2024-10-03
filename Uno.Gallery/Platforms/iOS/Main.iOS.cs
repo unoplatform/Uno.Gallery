fa4748136977a7f41077f103b00ce7faeeeb6ef1
Martin Zikmund <martin@zikmund.dev>
2024-10-03 11:48:33 +0200

    feat: iOS Skia support

--- Uno.Gallery/Platforms/iOS/Main.iOS.cs
+++ Uno.Gallery/Platforms/iOS/Main.iOS.cs
@@ -1,4 +1,5 @@
 using UIKit;
+using Uno.UI.Runtime.Skia.AppleUIKit;
 
 namespace Uno.Gallery
 {
@@ -7,9 +8,14 @@ namespace Uno.Gallery
 		// This is the main entry point of the application.
 		static void Main(string[] args)
 		{
+#if !IS_UIKIT_SKIA
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
 			UIApplication.Main(args, null, typeof(App));
+#else
+			var host = new PlatformHost(() => new App());
+			await host.Run();
+#endif
 		}
 	}
 }
