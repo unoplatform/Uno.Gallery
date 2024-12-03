6887c29cb8455a72d7476a01cc62b1e33e3e99bc
Jerome Laban <jerome@platform.uno>
2024-12-03 17:01:20 -0500

    chore: Adjust ios test

--- build/scripts/ios-uitest-build.sh
+++ build/scripts/ios-uitest-build.sh
@@ -5,4 +5,8 @@ IFS=$'\n\t'
 export UNO_UITEST_IOS_PROJECT=$BUILD_SOURCESDIRECTORY/Uno.Gallery
 
 cd $UNO_UITEST_IOS_PROJECT
-dotnet build -p:TargetFrameworkOverride=net9.0-ios -r iossimulator-x64 -c Release -p:IsUiAutomationMappingEnabled=True -bl:$BUILD_ARTIFACTSTAGINGDIRECTORY/ios-app.binlog
+
+########
+# Warning using net8.0-ios is required because of xamarin.uitest not supporting net9
+########
+dotnet build -p:TargetFrameworkOverride=net8.0-ios -r iossimulator-x64 -c Release -p:IsUiAutomationMappingEnabled=True -bl:$BUILD_ARTIFACTSTAGINGDIRECTORY/ios-app.binlog
