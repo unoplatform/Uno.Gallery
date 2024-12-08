5bd903f4f83cf9a59c2e267b7507fd55b9fc85b1
Jerome Laban <jerome@platform.uno>
2024-12-08 11:14:17 -0500

    chore: Roll back to net8 for ios tests compatibility

--- build/scripts/ios-uitest-build.sh
+++ build/scripts/ios-uitest-build.sh
@@ -5,4 +5,4 @@ IFS=$'\n\t'
 export UNO_UITEST_IOS_PROJECT=$BUILD_SOURCESDIRECTORY/Uno.Gallery
 
 cd $UNO_UITEST_IOS_PROJECT
-dotnet build -p:TargetFrameworkOverride=net9.0-ios -r iossimulator-x64 -c Release -p:IsUiAutomationMappingEnabled=True -bl:$BUILD_ARTIFACTSTAGINGDIRECTORY/ios-app.binlog
+dotnet build -p:TargetFrameworkOverride=net8.0-ios -r iossimulator-x64 -c Release -p:IsUiAutomationMappingEnabled=True -bl:$BUILD_ARTIFACTSTAGINGDIRECTORY/ios-app.binlog
