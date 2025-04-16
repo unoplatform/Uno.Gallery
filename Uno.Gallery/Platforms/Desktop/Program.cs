ac9e3f7af8b3746014b8ef4780e6f89b4bf57405
Jerome Laban <jerome@platform.uno>
2025-04-15 20:56:16 -0400

    chore: Always run win32

--- Uno.Gallery/Platforms/Desktop/Program.cs
+++ Uno.Gallery/Platforms/Desktop/Program.cs
@@ -14,11 +14,7 @@ public class Program
             .UseX11()
             .UseLinuxFrameBuffer()
             .UseMacOS()
-#if HAS_SKIA_RENDERER
             .UseWin32()
-#else
-            .UseWindows()
-#endif
             .Build();
 
         host.Run();
