79a0d017e341575c99ac07c65df99a278e6d222b
booskander <hannachiskander@live.ca>
2025-06-02 19:04:32 -0700

    ci: adding windows build metrics

--- build/scripts/collect-metrics-build.sh
+++ build/scripts/collect-metrics-build.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    echo "Usage: $0 <BUILD FOLDER>"
+    echo "Usage: $0 <PACKAGE FOLDER>"
     exit 1
 fi
 
-TARGET_FOLDER="$1"
-METRICS_FOLDER="$TARGET_FOLDER/metrics"
+if [ -z "$1" ]; then
+    echo "Usage: $0 <ARTIFACTS FOLDER>"
+    exit 1
+fi
+
+ARTIFACT_DIRECTORY="$1"
+PACKAGE_FOLDER="$2"
+METRICS_FOLDER="$ARTIFACT_DIRECTORY/metrics"
 
 
 mkdir -p "$METRICS_FOLDER"
 
-echo "[] Target folder: $TARGET_FOLDER exists"
+echo "[] Target folder: $ARTIFACT_DIRECTORY exists"
 echo "[] Measuring package size..."
 
-folder_size=$(du -sh --block-size=1M "$TARGET_FOLDER" | cut -f1)
-timestamp=$(date +"%T")
+folder_size=$(du -sh --block-size=1M "$PACKAGE_FOLDER" | cut -f1)
+timestamp=$(date + date '+%d/%m/%Y %H:%M:%S')
 
 echo "[] Writing output to metrics.json in $METRICS_FOLDER"
 
