133c7b0679670284dd65ee3f703a85159ee24f1c
agneszitte <agnes@platform.uno>
2026-02-24 16:27:13 -0500

    fix(ci): use wasm publish layout with _framework

--- build/scripts/wasm-uitest-build.sh
+++ build/scripts/wasm-uitest-build.sh
@@ -4,17 +4,15 @@ IFS=$'\n\t'
 
 export UNO_UITEST_SCREENSHOT_PATH=$BUILD_ARTIFACTSTAGINGDIRECTORY/screenshots/wasm
 export UNO_UITEST_WASM_PROJECT=$BUILD_SOURCESDIRECTORY/Uno.Gallery/Uno.Gallery.csproj
+export UNO_UITEST_WASM_PUBLISH_OUT=$BUILD_ARTIFACTSTAGINGDIRECTORY/wasm-uitest-publish
 
 cd $BUILD_SOURCESDIRECTORY
 
-dotnet publish -f net10.0-browserwasm -r browser-wasm -c Release $UNO_UITEST_WASM_PROJECT -p:TargetFrameworkOverride=net10.0-browserwasm -p:UseNativeRendering=true -p:IsUiAutomationMappingEnabled=True -bl:$UNO_UITEST_SCREENSHOT_PATH/msbuild.binlog
+dotnet publish -f net10.0-browserwasm -c Release $UNO_UITEST_WASM_PROJECT -p:UseNativeRendering=true -p:IsUiAutomationMappingEnabled=True -o "$UNO_UITEST_WASM_PUBLISH_OUT" -bl:$UNO_UITEST_SCREENSHOT_PATH/msbuild.binlog
 
-WASM_OUT="$BUILD_SOURCESDIRECTORY/Uno.Gallery/bin/Release/net10.0-browserwasm/publish"
-if [ ! -d "$WASM_OUT" ]; then
-  WASM_OUT="$BUILD_SOURCESDIRECTORY/Uno.Gallery/bin/Release/net10.0-browserwasm/publish"
-fi
+WASM_OUT="$UNO_UITEST_WASM_PUBLISH_OUT/wwwroot"
 
 echo "Contents of WASM output folder:"
-ls -la "$BUILD_SOURCESDIRECTORY/Uno.Gallery/bin/Release/net10.0-browserwasm/publish"
+ls -la "$WASM_OUT"
 
 echo "##vso[task.setvariable variable=UNO_UITEST_WASM_OUTPUT_PATH]$WASM_OUT"
